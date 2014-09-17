9bf2f0c8221fef39b6469d0177805bbbc46f2172
Yuriy Gerasimov <yuri.gerasimov@gmail.com>
2014-09-17 09:11:44 +0300

    Init commit. Page works after reinstall.

---
+++ drupal/sites/all/themes/custom/nas/layouts/site_template_small_header/site-template-small-header.tpl.php
@@ -0,0 +1,215 @@
+<body>
+  <header class="global-header alt standard">
+  <div class="row">
+  <div class="columns">
+    <a href="../../index.html" class="current-parent wordmark">Audubon</a>
+    <!-- Only visible on small and medium screens -->
+    <a href="#" class="icon-menu header-btn header-btn-menu"></a>
+    <!-- / -->
+    <div class="global-nav">
+      <div class="mobile-nav">
+        <ul class="primary-nav inline-list">
+          <li class="primary-nav-item">
+            <a class="primary-nav-toggler" href="../../news/index.html">News</a>
+            <ul class="primary-sub-nav">
+              <li class="primary-sub-nav-item primary-sub-nav-index"><a class="primary-sub-nav-link" href="../../news/index.html">News Home</a></li>
+              <li class="primary-sub-nav-item"><a class="primary-sub-nav-link" href="#">Birds in the News</a></li>
+              <li class="primary-sub-nav-item"><a class="primary-sub-nav-link" href="#">Inside Audubon</a></li>
+              <li class="primary-sub-nav-item"><a class="primary-sub-nav-link" href="#">Featured Events</a></li>
+              <li class="primary-sub-nav-item"><a class="primary-sub-nav-link" href="#">Audubon Climate Watch</a></li>
+            </ul>
+          </li>
+          <li class="primary-nav-item">
+            <a class="primary-nav-toggler" href="#">Features</a>
+            <ul class="primary-sub-nav">
+              <li class="primary-sub-nav-item primary-sub-nav-index"><a class="primary-sub-nav-link" href="#">Features Home</a></li>
+              <li class="primary-sub-nav-item"><a class="primary-sub-nav-link" href="#">Map Stories</a></li>
+              <li class="primary-sub-nav-item"><a class="primary-sub-nav-link" href="#">From the Magazine</a></li>
+              <li class="primary-sub-nav-item"><a class="primary-sub-nav-link" href="#">Videos</a></li>
+            </ul>
+          </li>
+          <li class="current-parent primary-nav-item">
+            <a class="current-parent primary-nav-toggler" href="../index.html">Birds</a>
+            <ul class="primary-sub-nav">
+              <li class="current-parent primary-sub-nav-item primary-sub-nav-index"><a class="current-parent primary-sub-nav-link" href="../index.html">Birds Home</a></li>
+              <li class="primary-sub-nav-item"><a class="primary-sub-nav-link" href="#">Flyways</a></li>
+              <li class="current-parent current primary-sub-nav-item"><a class="current-parent current primary-sub-nav-link" href="index.html">Bird Guide</a></li>
+              <li class="primary-sub-nav-item"><a class="primary-sub-nav-link" href="#">Priority Birds</a></li>
+              <li class="primary-sub-nav-item"><a class="primary-sub-nav-link" href="#">Webcams</a></li>
+              <li class="primary-sub-nav-item"><a class="primary-sub-nav-link" href="#">Birds of America</a></li>
+            </ul>
+          </li>
+          <li class="primary-nav-item">
+            <a class="primary-nav-toggler" href="../../conservation/index.html">Conservation</a>
+            <ul class="primary-sub-nav">
+              <li class="primary-sub-nav-item primary-sub-nav-index"><a class="primary-sub-nav-link" href="../../conservation/index.html">Conservation Home</a></li>
+              <li class="primary-sub-nav-item"><a class="primary-sub-nav-link" href="#">Science</a></li>
+              <li class="primary-sub-nav-item"><a class="primary-sub-nav-link" href="#">Advocacy</a></li>
+              <li class="primary-sub-nav-item"><a class="primary-sub-nav-link" href="#">Important Bird Areas</a></li>
+              <li class="primary-sub-nav-item"><a class="primary-sub-nav-link" href="#">Bird Friendly Communities</a></li>
+              <li class="primary-sub-nav-item"><a class="primary-sub-nav-link" href="#">Climate &amp; Energy</a></li>
+              <li class="primary-sub-nav-item"><a class="primary-sub-nav-link" href="#">Seas &amp; Shores</a></li>
+              <li class="primary-sub-nav-item"><a class="primary-sub-nav-link" href="#">Working Lands</a></li>
+              <li class="primary-sub-nav-item"><a class="primary-sub-nav-link" href="#">Education</a></li>
+            </ul>
+          </li>
+          <li class="primary-nav-item">
+            <a class="primary-nav-toggler" href="../../getoutside/index.html">Get Outside</a>
+            <ul class="primary-sub-nav">
+              <li class="primary-sub-nav-item primary-sub-nav-index"><a class="primary-sub-nav-link" href="../../getoutside/index.html">Get Outside Home</a></li>
+              <li class="primary-sub-nav-item"><a class="primary-sub-nav-link" href="#">Birding</a></li>
+              <li class="primary-sub-nav-item"><a class="primary-sub-nav-link" href="#">Photography</a></li>
+              <li class="primary-sub-nav-item"><a class="primary-sub-nav-link" href="#">Travel</a></li>
+              <li class="primary-sub-nav-item"><a class="primary-sub-nav-link" href="#">Activities</a></li>
+            </ul>
+          </li>
+          <li class="primary-nav-item"><a href="#">About</a></li>
+          <li class="primary-nav-item primary-nav-search">
+            <a class="orange" href="#">Donate</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  </div>
+    <?php print $content['header']; ?>
+    <?php print $content['main']; ?>
+  
+<div class="row">
+  <div class="card-set-social social-sharing">
+    <span class="social-sharing-caption white">Spread the word. It&rsquo;s the least you can do.</span>
+    <a class="social-sharing-icon white" href="#"><i class="icon-twitter"></i></a>
+    <a class="social-sharing-icon white" href="#"><i class="icon-facebook"></i></a>
+    <a class="social-sharing-icon white" href="#"><i class="icon-mail"></i></a>
+  </div>
+</div>
+</section>
+  </section>
+  <aside class="mailing-list">
+<div class="row">
+  <div class="column">
+    <div class="mailing-list-text">
+      <h2 class="mailing-list-headline">Stay abreast of Audubon</h2>
+      <p class="mailing-list-caption"><em>Our email newsletter shares the latest programs and initiatives.</em></p>
+    </div><form class="mailing-list-form">
+      <input class="mailing-list-input radius" placeholder="Enter your email address" type="text">
+      <input class="button tomato large" type="submit" value="Sign Up">
+    </form>
+  </div>
+</div>
+</aside>
+  <footer class="global-footer">
+    <?php print $content['footer']; ?>
+    <section class="footer-site-map">
+    <div class="row">
+      <div class="medium-12 large-3 columns">
+        <img class="footer-logo" src="../../../img/footer-logo.png" alt="">
+        <h5>National Audubon Society</h5>
+        <p>Our mission is to conserve and restore natural ecosystems, focusing on birds, other wildlife,  and their habitats for the benefit of humanity and the earth’s biological diversity.</p>
+      </div>
+      <div class="tiny-6 medium-4 large-3 columns">
+        <ul class="footer-nav">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">News</a></li>
+          <li><a href="#">Features</a></li>
+          <li><a href="#">Birds</a></li>
+          <li><a href="#">Conservation</a></li>
+          <li><a href="#">Get Outside</a></li>
+          <li><a href="#">Magazine</a></li>
+        </ul>
+      </div>
+      <div class="tiny-6 medium-4 large-3 columns">
+        <ul class="footer-nav">
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Newsroom</a></li>
+          <li><a href="#">Audubon Campus</a></li>
+          <li><a href="#">Careers</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </div>
+      <div class="medium-4 large-3 columns">
+        <ul class="footer-nav">
+          <li><a href="#">Join the National Audubon Society</a></li>
+          <li><a href="#">Update Your Information</a></li>
+          <li><a href="#">Member Services</a></li>
+          <li><a href="#">How to Help</a></li>
+          <li><a href="#">Donate to Audubon</a></li>
+          <li><a href="#">Online Store</a></li>
+          <li><a href="#">Audubon Near You</a></li>
+          <li><a href="#">Find a Local Chapter</a></li>
+        </ul>
+      </div>
+    </div>
+  </section>
+  <section class="footer-copyright">
+    <div class="row">
+      <div class="column">
+        <p>Copyright © 2014 National Audubon Society, Inc. <span class="footer-copyright-links"><a href="#">Legal Notices</a> <a href="#">Privacy Policy</a> <a href="#">Contact Us</a></span></p>
+      </div>
+    </div>
+  </section>
+</footer>
+  <script src="../../../js/iscroll.js"></script>
+<script src="../../../js/nav.js"></script>
+<script src="../../../js/jquery.mousewheel.js"></script>
+<script src="../../../js/jquery.colorbox-min.js"></script>
+<script src="../../../js/scrolljack.js"></script>
+<script src="../../../js/card-equalizer.js"></script>
+<script src="../../../js/article-reflow.js"></script>
+<script src="../../../js/curtain.js"></script>
+<script src="../../../js/bird-card-set.js"></script>
+<script src="../../../js/strip-nav.js"></script>
+<script src="../../../js/card-set.js"></script>
+<script src="../../../js/thumb-strip.js"></script>
+<script src="../../../js/slideshow.js"></script>
+<script src="../../../js/conservation-slider.js"></script>
+<script src="../../../js/bg-respond.js"></script>
+<script src="../../../js/bird-guide-respond.js"></script>
+<script src="../../../js/toggler.js"></script>
+<script src="../../../js/inline-map.js"></script>
+<script src="../../../js/responsive-events.js"></script>
+<script src="../../../js/responsive-tables.js"></script>
+<script src="../../../js/bird-guide-grid.js"></script>
+<script src="../../../js/flyway-minimap.js"></script>
+<script src="../../../js/flyway-slides.js"></script>
+<script src="../../../js/pledge-form-alt.js"></script>
+<script src="../../../js/expand-hero.js"></script>
+
+
+  <!-- DEMO SCRIPT -->
+  <script>
+
+    $(".load-more").bind("click", function(e) {
+      e.preventDefault();
+      var $this = $(this),
+          $container = $(".bird-card-grid-container"),
+          $cards = $container.contents(".columns").slice(0, 12).clone().addClass("invisible");
+
+      $this.addClass("active");
+
+      $(".bg-egg").css("display", "none");
+
+      setTimeout(function() {
+        $container.append($cards);
+        $cards.removeClass("invisible");
+        $this.removeClass("active");
+      }, 1500);
+    });
+
+    $(".bird-guide-search select").bind("change", function(e) {
+      var $this = $(this),
+          $container = $(".bird-card-grid-container"),
+          $cards = $container.contents(".columns").slice(0, 12).clone();
+
+      $container.find(".bird-card").addClass("disappear");
+      $(".bird-card.disappear").bind("animationend webkitAnimationEnd oanimationend MSAnimationEnd", function() {
+        $(this).addClass("invisible");
+      });
+      setTimeout(function() {
+        $(".bird-card-grid .section-header h2").text("Matching Birds");
+        $container.children().remove();
+        $container.append($cards);
+      }, 1500);
+    });
+
+  </script>
