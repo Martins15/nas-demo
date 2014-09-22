c89488632e8d6f3652c3f20136a0bd4f941b418c
Vadim Roscovan <roshkovanv@gmail.com>
2014-09-22 14:08:24 +0300

    ANW-127

---
+++ drupal/sites/all/modules/custom/nas_footer/templates/nas-footer-copyright-menu.tpl.php
@@ -0,0 +1,5 @@
+<span class="footer-copyright-links">
+  <?php foreach ($menu_tree as $menu_item) : ?>
+    <?php print l($menu_item['link']['link_title'], $menu_item['link']['link_path']); ?>
+  <?php endforeach; ?>
+</span>
---
+++ drupal/sites/all/modules/custom/nas_footer/templates/nas-footer.tpl.php
@@ -0,0 +1,20 @@
+<section class="footer-site-map">
+  <div class="row">
+    <div class="medium-12 large-3 columns">
+      <img class="footer-logo" src="/sites/all/themes/custom/nas/img/footer-logo.png" alt="">
+      <h5><?php print $mission_title; ?></h5>
+      <p><?php print $mission; ?></p>
+    </div>
+    <?php print $footer_menu; ?>
+  </div>
+</section>
+<section class="footer-copyright">
+  <div class="row">
+    <div class="column">
+      <p>
+        <?php print $copyright; ?>
+        <span class="footer-copyright-links"><?php print $footer_copyright_menu; ?></span>
+      </p>
+    </div>
+  </div>
+</section>
