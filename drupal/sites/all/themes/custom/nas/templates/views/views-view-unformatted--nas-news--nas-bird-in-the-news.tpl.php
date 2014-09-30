53371c12e7a1673068803ca1ca8073095f516e1c
atiupa <redfoxxx.ua@gmail.com>
2014-09-30 11:48:23 +0300

    [ANW-112] Created a new module nas_panes for storing and handling custom panel panes plugins, added 1st ctools content type Bird In The News with advanced settings support

--- drupal/sites/all/themes/custom/nas/templates/views/views-view-unformatted--nas-news--nas-bird-in-the-news.tpl.php
+++ drupal/sites/all/themes/custom/nas/templates/views/views-view-unformatted--nas-news--nas-bird-in-the-news.tpl.php
@@ -7,6 +7,16 @@
  * @ingroup views_templates
  */
 ?>
+<div class="row section-header">
+  <div class="columns">
+    <h2 class="thin"><?php print drupal_get_title() . '&nbsp;' . t('in the News'); ?></h2>
+  </div>
+  <div class="columns">
+    <ul class="section-nav inline-list hide-for-small hide-for-tiny">
+      <li><?php print l(t('More News »'), ''); ?></a></li>
+    </ul>
+  </div>
+</div>
 <div class="row">
   <?php foreach ($rows as $id => $row): ?>
   <div class="large-4 columns">
