800f283f8670d53a162ed16a9a0a4e7a595d46e1
Yuriy Gerasimov <yuri.gerasimov@gmail.com>
2014-10-01 13:09:55 +0300

    Theme contextual link. Comments in template file.

--- drupal/sites/all/modules/custom/nas_panes/plugins/content_types/featured_bird/nas-featured-bird.tpl.php
+++ drupal/sites/all/modules/custom/nas_panes/plugins/content_types/featured_bird/nas-featured-bird.tpl.php
@@ -2,6 +2,16 @@
 /**
  * @file
  * Featured Bird template file.
+ *
+ * Available variables:
+ * - $name: the (sanitized) name of the bird.
+ * - $url: url of the link to bird's individual page.
+ * - $scientific_name: the (sanitized) scientific name of the bird.
+ * - $image_path: path to the hero image of the bird.
+ * - $image_path_mobile: path to the hero image of the bird mobile image style.
+ * - $image_string: sanitized caption + credits of the hero image.
+ * - $illustration_path: path to the illustration image of the bird.
+ * - $contextual_links: rendered contextual links.
  */
 ?>
 <div class="hero light-text dark-gradient">
