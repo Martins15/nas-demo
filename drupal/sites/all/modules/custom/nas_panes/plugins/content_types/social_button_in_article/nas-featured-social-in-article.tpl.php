401b11c410a0af422439fc8b198e775b5f67a768
sempay <serjuk02@gmail.com>
2014-10-09 10:36:57 +0300

    Added block with social button in page Article ANW-172

---
+++ drupal/sites/all/modules/custom/nas_panes/plugins/content_types/social_button_in_article/nas-featured-social-in-article.tpl.php
@@ -0,0 +1,5 @@
+<section class="clearfix article-sidebar-section social-sharing no-caption hide-for-tiny hide-for-small hide-for-medium">
+  <a class="social-sharing-icon" target="_blank" href="http://twitter.com/share?url=/&amp;text=<?php print $page_link; ?>"><i class="icon-twitter"></i></a>&nbsp;
+  <a class="social-sharing-icon" target="_blank" href="http://www.facebook.com/sharer/sharer.php?u=<?php print $page_link; ?>"><i class="icon-facebook"></i></a>&nbsp;
+  <a class="social-sharing-icon" href="mailto:?subject=<?php print $title; ?>&body=<?php print $page_link; ?>"><i class="icon-mail"></i></a>
+</section>
