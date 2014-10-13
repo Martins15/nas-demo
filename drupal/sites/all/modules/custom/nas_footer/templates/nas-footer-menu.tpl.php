d97af5424a4d42c87e9d6d4ed15ff26a846b2116
Vadim Roscovan <vadim.roscovan@workingpropeople.com>
2014-10-13 15:45:21 +0300

    fix PR notices

--- drupal/sites/all/modules/custom/nas_footer/templates/nas-footer-menu.tpl.php
+++ drupal/sites/all/modules/custom/nas_footer/templates/nas-footer-menu.tpl.php
@@ -2,6 +2,38 @@
 /**
  * @file
  * Template for footer menu.
+ * 
+ * Available variables:
+ * - $menu_tree: menu items. Example
+ *  $menu_tree = array(
+ * 'item 1' => array(
+ *   'link' => array(
+ *     'link_path' => '<front>',
+ *     'link_title' => 'Item 1',
+ *     'options' => array(
+ *       'attributes' => array(
+ *         'class' => array(
+ *             'tiny-6',
+ *             'medium-4',
+ *             'large-3',
+ *             'columns',
+ *         )
+ *       )
+ *     )
+ *   ),
+ *   'below' => array(
+ *     '49950 Home 259' => array(
+ *       'link' => array(
+ *         'link_path' => '<front>',
+ *         'link_title' => 'Home',
+ *         'options' => array(
+ *           'attributes' => array()
+ *         )
+ *       )
+ *     )
+ *   )
+ * )
+ * );
  */
 ?>
 
@@ -12,7 +44,7 @@
       <ul class="footer-nav">
         <?php foreach ($menu_item['below'] as $sub_menu_item) : ?>
           <li>
-            <a href="<?php print url($sub_menu_item['link']['link_path']);?>">
+            <a href="<?php print url($sub_menu_item['link']['link_path']); ?>">
               <?php print check_plain($sub_menu_item['link']['link_title']); ?>
             </a>
           </li>
