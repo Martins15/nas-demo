732e3e350a8fd072a5db3432e73ab12f74149193
sempay <serjuk02@gmail.com>
2014-10-20 10:50:37 +0300

    fixed [ANW-186]

--- drupal/sites/all/modules/custom/nas_footer/templates/nas-footer-copyright-menu.tpl.php
+++ drupal/sites/all/modules/custom/nas_footer/templates/nas-footer-copyright-menu.tpl.php
@@ -7,8 +7,6 @@
 
 <span class="footer-copyright-links">
   <?php foreach ($menu_tree as $menu_item) : ?>
-  	<?php if ($menu_item['link']['hidden'] == FALSE) : ?>
-    	<?php print l($menu_item['link']['link_title'], $menu_item['link']['link_path']); ?>
-	<?php endif;?>
+      <?php print l($menu_item['link']['link_title'], $menu_item['link']['link_path']); ?>
   <?php endforeach; ?>
 </span>
