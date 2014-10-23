0b56e8f32ddb43456872d974a64652cedcc8bee3
Yuriy Gerasimov <yuri.gerasimov@gmail.com>
2014-10-23 12:58:13 +0300

    Add custom pane with Press Contact.

---
+++ drupal/sites/all/modules/custom/nas_panes/plugins/content_types/press_contact/nas-panes-press-contact.tpl.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @file
+ * Press contact block.
+ *
+ * Available variables:
+ * - $name: name of the person.
+ * - $title: person's title.
+ * - $address: office address.
+ * - $phone: phone number.
+ */
+?>
+<section class="medium-12 clearfix article-sidebar-section contact-info">
+  <h5><?php print $title; ?></h5>
+  <?php foreach ($contacts as $contact): ?>
+    <p><strong><?php print $contact['name']; ?></strong><br>
+      <?php if (!empty($contact['position'])): ?>
+        <?php print $contact['position']; ?><br/>
+      <?php endif;?></p>
+      <?php if (!empty($contact['address'])): ?>
+        <p><?php print $contact['address']; ?></p>
+      <?php endif;?>
+      <?php if (!empty($contact['email'])): ?>
+        <p><a href="mailto:<?php print $contact['email']; ?>"><?php print $contact['email']; ?></a></p>
+      <?php endif;?>
+      <?php if (!empty($contact['phone'])): ?>
+        <p><?php print $contact['phone']; ?></p>
+      <?php endif;?>
+  <?php  endforeach; ?>
+</section>
