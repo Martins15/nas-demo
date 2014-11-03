5c907592ad0691a81a884e15d9758747bcd8c447
Yuriy Gerasimov <yuri.gerasimov@gmail.com>
2014-11-03 17:02:02 +0200

    A view to display news articles by flyway. Custom override.

--- drupal/sites/all/modules/custom/nas_panes/plugins/content_types/flyways_landing_bottom/flyways-landing-bottom.tpl.php
+++ drupal/sites/all/modules/custom/nas_panes/plugins/content_types/flyways_landing_bottom/flyways-landing-bottom.tpl.php
@@ -22,7 +22,8 @@
   </div>
 </section>
 
-<div class="flyway-slides">
+<div class="flyway-slides contextual-links-region">
+  <?php print $contextual_links; ?>
   <div class="flyway-slides-container">
     <?php for ($i = 1; $i <= NAS_FLYWAYS_NUMBER; $i++): ?>
     <div id="<?php print $flyway[$i]['name_id']; ?>-slide" class="flyway-slide<?php if ($i == 1 ): ?> current<?php endif; ?>">
@@ -46,42 +47,7 @@
         </div>
       </div>
       <div class="row space-top space-bottom">
-        <div class="columns large-4">
-          <div class="editorial-card collapse-minimal">
-            <div class="editorial-card-photo">
-              <img src="../../img/editorial-card-1.jpg">
-            </div>
-            <div class="editorial-card-content short">
-              <a href="#" class="editorial-card-slug">Priority Birds</a>
-              <h4 class="editorial-card-title"><a href="#">Greater Sage Grouse</a></h4>
-              <p>The flyway is home to 17 Priority Bird species, depend on a wide range of habitats.</p>
-            </div>
-          </div>
-        </div>
-        <div class="columns large-4">
-          <div class="editorial-card collapse-minimal">
-            <div class="editorial-card-photo">
-              <img src="../../img/editorial-card-2.jpg">
-            </div>
-            <div class="editorial-card-content short">
-              <a href="#" class="editorial-card-slug">Important Bird Areas</a>
-              <h4 class="editorial-card-title"><a href="#">Aiken Canyon Preserve</a></h4>
-              <p>The United States region of the Central Flyway is home to 65 global Important Bird Areas.</p>
-            </div>
-          </div>
-        </div>
-        <div class="columns large-4">
-          <div class="editorial-card collapse-minimal">
-            <div class="editorial-card-photo">
-              <img src="../../img/editorial-card-3.jpg">
-            </div>
-            <div class="editorial-card-content short">
-              <a href="#" class="editorial-card-slug">In The News</a>
-              <h4 class="editorial-card-title"><a href="#">Improving the Health of Rivers in the Mid-Atlantic</a></h4>
-              <p>April 23, 2014</p>
-            </div>
-          </div>
-        </div>
+        <?php print $flyway[$i]['news_teasers']; ?>
       </div>
     </div>
     <?php endfor; ?>
