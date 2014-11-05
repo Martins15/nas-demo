a70f60a8ad6cff9045e8ab9200bf5a0d60061583
Alexander Schedrov <alexander.schedrov@gmail.com>
2014-11-05 11:58:13 +0200

    [ANW-133] Added templates for view.

---
+++ drupal/sites/all/themes/custom/nas/templates/views/views-view-unformatted--nas-birds-family--nas-birds-family.tpl.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file
+ * Custom view template to display Birds Family.
+ */
+?>
+
+<div class="row section-header space-top">
+  <div class="columns">
+    <h2 class="thin"><?php print $view->display[$view->current_display]->display_title; ?></h2>
+  </div>
+</div>
+
+<div class="view-content row space-bottom double bird-card-grid-container">
+  <?php foreach ($rows as $id => $row): ?>
+    <div class="columns tiny-6 medium-4 large-3">
+    <?php print $row; ?>
+    </div>
+  <?php endforeach; ?>
+</div>
