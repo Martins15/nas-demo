ff03a2ec88148f13c58221443c2dffc1581adf9b
atiupa <redfoxxx.ua@gmail.com>
2014-11-06 21:49:51 +0200

    [ANW-250] add Get Outside plugin

---
+++ drupal/sites/all/modules/custom/nas_panes/plugins/content_types/get_outside/nas-get-outside.tpl.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @file
+ * Featured Bird template file.
+ *
+ * Available variables:
+ * - $image_path: path to the hero image.
+ * - $contextual_links: rendered contextual links.
+ * - $color_classes: color classes for text.
+ * - $text_array: array of titles and texts.
+ */
+?>
+
+<div class="breakout-section no-hero contextual-links-region" style="background-image: url(<?php print $image_path; ?>); background-color: #444444">
+  <?php print $contextual_links; ?>
+  <div class="breakout-section-content <?php print $color_classes; ?>">
+    <div class="row space-top space-bottom double">
+      <div class="columns large-5 text-container">
+        <h1 class="thin"><?php print $title; ?></h1>
+        <br>
+        <?php foreach ($text_array as $value) : ?>
+        <h2 class="thin close-heading"><?php print $value['title']; ?></h2>
+        <?php print $value['text']['value']; ?>
+        <?php endforeach; ?>
+      </div>
+    </div>
+  </div>
+</div>
