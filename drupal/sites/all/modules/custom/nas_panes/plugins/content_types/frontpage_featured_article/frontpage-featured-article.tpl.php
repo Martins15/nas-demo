2d032e1bdd60115e19bc612322b754e3a354d596
andriyun <andriy.yun@gmail.com>
2014-11-06 11:03:15 +0000

    [ANW-273] Based implementation

---
+++ drupal/sites/all/modules/custom/nas_panes/plugins/content_types/frontpage_featured_article/frontpage-featured-article.tpl.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @file
+ * Featured Frontpage template file.
+ *
+ * Available variables:
+ * - $title_link: the (sanitized) title with link.
+ * - $blue_text_link: the (sanitized) blue text above the title with link.
+ * - $image_path: path to the hero image of the bird.
+ * - $image_path_mobile: path to the hero image of the bird mobile image style.
+ * - $image_string: sanitized caption + credits of the hero image.
+ * - $contextual_links: rendered contextual links.
+ */
+?>
+  <div class="columns large-8 medium-6">
+    <div class="editorial-card">
+      <div class="editorial-card-photo">
+        <img src="../img/editorial-card-23.jpg" alt="">
+      </div>
+      <div class="editorial-card-content short">
+        <a href="#" class="editorial-card-slug">Conservation</a>
+        <h3 class="editorial-card-title"><a href="#">A Trove of Green Heron Nests Might Just be a Treasure</a></h4>
+        <p><a href="#" class="editorial-card-link">Recent discoveries help scientists understand the elusive bird's decline</a></p>
+      </div>
+    </div>
+  </div>
