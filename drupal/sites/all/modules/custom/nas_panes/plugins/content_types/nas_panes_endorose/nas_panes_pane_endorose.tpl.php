585b76bf88a6c2216b9aa401ea28925c8eaa2b92
sempay <serjuk02@gmail.com>
2014-11-07 05:32:07 +0200

    add plagin to panels and settings him ANW-256

---
+++ drupal/sites/all/modules/custom/nas_panes/plugins/content_types/nas_panes_endorose/nas_panes_pane_endorose.tpl.php
@@ -0,0 +1,12 @@
+<div class="engagement-card">
+  <div class="engagement-card-content no-min-height">
+    <h3 class="engagement-card-headline">Help the Plight of the Albatross</h3>
+    <p>These magnificent birds die tragic deaths, becoming entangled in fishing lines.</p>
+    <div class="engagement-card-cta">
+      <a href="#" class="button tomato xlarge">Endorse New Legislation</a>
+    </div>
+  </div>
+  <div class="engagement-card-photo">
+    <img src="<?php print($path);?>" alt="">
+  </div>
+</div>
---
+++ drupal/sites/all/modules/custom/nas_panes/plugins/content_types/nas_panes_welcome_to_audubon/nas_panes_welcome_to_audubon.tpl.php
@@ -0,0 +1,28 @@
+<div class="welcome-card teal">
+  <div class="welcome-card-illustration">
+    <img src="http://188.226.239.231/Prototypes/pages/Build/img/welcome-illustration-teal.jpg">
+  </div>
+  <div class="welcome-card-content">
+    <div class="welcome-card-headline">
+      Welcome to<br>
+      <img class="logo" src="http://188.226.239.231/Prototypes/pages/Build/img/wordmark-black.png" alt="">
+    </div>
+    <div class="welcome-card-blurb">
+      <p><em><strong>Our Mission:</strong> To conserve and restore natural ecosystems, focusing on birds, other wildlife, and their habitats for the benefit of humanity and the earth's biological diversity. <a href="#">Learn more »</a></em></p>
+      <hr>
+    </div>
+    <a href="#" class="editorial-card-slug">In the News</a>
+    <h5 class="editorial-card-title"><a href="#">First Bird Death Toll Estimate from the BP Oil Spill</a></h5>
+    <a href="#" class="editorial-card-slug">Most Popular</a>
+    <h5 class="editorial-card-title"><a href="#">A Gazillion Reasons Migratory Birds Totally Rock</a></h5>
+  </div>
+  <div class="welcome-card-banner">
+    <div class="social-sharing">
+      <span class="social-sharing-caption small">Follow us </span>
+      <a class="social-sharing-icon blue small" target="_blank" href="http://twitter.com/share?url=/&amp;text=<?php print $page_link; ?>"><i class="icon-twitter"></i></a>&nbsp;
+      <a class="social-sharing-icon blue small" target="_blank" href="http://www.facebook.com/sharer/sharer.php?u=<?php print $page_link; ?>"><i class="icon-facebook"></i></a>&nbsp;
+      <a class="social-sharing-icon small" href="#"><i class="icon-youtube"></i></a>
+      <a class="social-sharing-icon blue small" target="_blank" href="http://pinterest.com/pin/create/button/?url=<?php print $page_link; ?>"><i class="icon-pinterest"></i></a>&nbsp;
+    </div>
+  </div>
+</div>
