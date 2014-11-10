7327d5dc48c8257f791dc114a1ebc1a23a9e3e16
sempay <serjuk02@gmail.com>
2014-11-10 17:07:45 +0200

    fix standart ANW-256

--- drupal/sites/all/modules/custom/nas_wysiwyg/theme/bean--welcome_to_audobun.tpl.php
+++ drupal/sites/all/modules/custom/nas_wysiwyg/theme/bean--welcome_to_audobun.tpl.php
@@ -35,7 +35,7 @@
     <div class="welcome-card-content">
       <div class="welcome-card-headline">
         Welcome to<br>
-        <img class="logo" src="<?php print($path_to_logo);?>" alt="">
+        <img class="logo" src="<?php print ($path_to_logo);?>" alt="">
       </div>
       <div class="welcome-card-blurb">
         <?php print ($summary);?>
