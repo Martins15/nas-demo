752b16008139249259b44f71942872f07e7c5cca
Andrey Maximov <savagge@gmail.com>
2014-11-11 17:28:26 +0300

    [ANW-272] Remove Popular Stories from Press Release layout. Hide comment settings if Press Release selected on node edit form.

---
+++ drupal/sites/all/modules/custom/nas_alters/js/hide-comment-settings.js
@@ -0,0 +1,23 @@
+(function ($) {
+  Drupal.behaviors.hide_comment_settings = {
+    attach: function (context, settings) {
+      $('#edit-panelizer-page-manager-name', context).once('hide-comments', function () {
+        var page_manager_el = $(this);
+        $(this).bind('change', function () {
+            // Get index of comment settings group.
+            var index = $('#edit-comment-settings').index();
+            var vertical_tabs = $('#edit-comment-settings').parents('.vertical-tabs');
+            // Hide comment settings vertical tab if press release layout
+            // selected.
+            if (page_manager_el.val() == 'node:article:press_release') {
+              $('.vertical-tabs-list .vertical-tab-button:eq('+index+')', vertical_tabs).hide();
+            }
+            else {
+              $('.vertical-tabs-list .vertical-tab-button:eq('+index+')', vertical_tabs).show();
+            }
+          })
+          .trigger('change');
+      });
+    }
+  }
+})(jQuery);
