3788667f3d3d30f9be4cc7c019b46d79935bc553
Alexander Schedrov <alexander.schedrov@gmail.com>
2014-11-12 11:22:55 +0200

    [ANW-292] Added layout for slideshow page.

---
+++ drupal/sites/all/themes/custom/nas/layouts/nas_slideshow/nas-slideshow.tpl.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file
+ * Template implementation to display the panel's layout.
+ */
+?>
+
+<?php print $content['header']; ?>
+<?php print $content['body']; ?>
+<?php print $content['bottom']; ?>
