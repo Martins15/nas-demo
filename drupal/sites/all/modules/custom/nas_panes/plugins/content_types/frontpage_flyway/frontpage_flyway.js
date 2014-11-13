cf465d8458f587a81cf6f48cf1dd148e2537d212
atiupa <redfoxxx.ua@gmail.com>
2014-11-13 13:14:08 +0200

    [ANW-247] add behaviors

--- drupal/sites/all/modules/custom/nas_panes/plugins/content_types/frontpage_flyway/frontpage_flyway.js
+++ drupal/sites/all/modules/custom/nas_panes/plugins/content_types/frontpage_flyway/frontpage_flyway.js
@@ -1,22 +1,25 @@
 (function($) {
-  var onSuccess = function(location) {
-    var locationObject = $.parseJSON(JSON.stringify(location, undefined, 4));
-    console.log("Lookup successful:");
-    console.log(locationObject);
-    var StateIsoCode = locationObject.subdivisions['0'].iso_code;
-    if (typeof(StateIsoCode) === 'undefined') {
-      StateIsoCode = 'default';
+  Drupal.behaviors.frontpage_flyway = {
+    attach: function(context, settings) {
+      var onSuccess = function(location) {
+        var locationObject = $.parseJSON(JSON.stringify(location, undefined, 4));
+        console.log("Lookup successful:");
+        console.log(locationObject);
+        var StateIsoCode = locationObject.subdivisions['0'].iso_code;
+        if (typeof(StateIsoCode) === 'undefined') {
+          StateIsoCode = 'default';
+        }
+        var ajax = new Drupal.ajax(false, false, {
+          url: "ajax/get/fpp/" + StateIsoCode
+        });
+        ajax.eventResponse(ajax, {});
+      };
+
+      var onError = function(error) {
+        console.log("Error:\n\n" + JSON.stringify(error, undefined, 4));
+      };
+
+      geoip2.city(onSuccess, onError);
     }
-    var ajax = new Drupal.ajax(false, false, {
-      url: "ajax/get/fpp/" + StateIsoCode
-    });
-    ajax.eventResponse(ajax, {});
-  };
-
-  var onError = function(error) {
-    console.log("Error:\n\n" + JSON.stringify(error, undefined, 4));
-  };
-
-  geoip2.city(onSuccess, onError);
-
+  }
 })(jQuery);
