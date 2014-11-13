29c2604b968d0e9fec535e06827b3f02428926de
Andrey Maximov <savagge@gmail.com>
2014-11-13 16:58:41 +0300

    [ANW-318] Frontpage in the news markup adjusted.

--- drupal/sites/all/modules/custom/nas_panes/plugins/content_types/frontpage_in_the_news/frontpage-in-the-news.tpl.php
+++ drupal/sites/all/modules/custom/nas_panes/plugins/content_types/frontpage_in_the_news/frontpage-in-the-news.tpl.php
@@ -11,30 +11,35 @@
  */
 ?>
 
-<div class="row section-header">
-  <div class="columns">
-    <h2 class="thin"><?php print $title; ?></h2>
-  </div>
-  <div class="columns">
-    <ul class="section-nav inline-list">
-      <li class="hide-for-small hide-for-tiny"><a href="<?php print $bird_guide_link; ?>"><?php print $bird_guide_title; ?></a></li>
-      <li><a class="cta" href="#">Adopt a Bird</a></li>
-    </ul>
-  </div>
-</div>
-<div class="row bird-card-container">
-  <div class="bird-card-scroller">
-    <?php for ($i = 0; $i < 4; $i++): ?>
-    <?php if (!isset($teasers[$i])) { continue; } ?>
-    <div class="columns tiny-3">
-      <?php print $teasers[$i]['bird']; ?>
-      <?php if (!empty($teasers[$i]['blue_link_url'])): ?>
-      <a href="<?php print $teasers[$i]['blue_link_url']; ?>" class="editorial-card-slug"><?php print $teasers[$i]['blue_link_text']; ?></a>
-      <?php endif; ?>
-      <h4 class="editorial-card-title"><a href="<?php print $teasers[$i]['article_url']; ?>"><?php print $teasers[$i]['article_title']; ?></a></h4>
+<div class="bird-card-set bg-bone-white bg-boa">
+  <div class="row section-header">
+    <div class="columns">
+      <h2 class="thin"><?php print $title; ?></h2>
     </div>
-    <?php endfor; ?>
+    <div class="columns">
+      <ul class="section-nav inline-list">
+        <li class="hide-for-small hide-for-tiny"><a href="<?php print $bird_guide_link; ?>"><?php print $bird_guide_title; ?></a></li>
+        <li><a class="cta" href="#">Adopt a Bird</a></li>
+      </ul>
+    </div>
+  </div>
+  <div class="row bird-card-container">
+    <div class="bird-card-scroller">
+      <?php for ($i = 0; $i < 4; $i++): ?>
+      <?php if (!isset($teasers[$i])) { continue; } ?>
+      <div class="columns tiny-3">
+        <?php print $teasers[$i]['bird']; ?>
+        <?php if (!empty($teasers[$i]['blue_link_url'])): ?>
+        <a href="<?php print $teasers[$i]['blue_link_url']; ?>" class="editorial-card-slug"><?php print $teasers[$i]['blue_link_text']; ?></a>
+        <?php endif; ?>
+        <h4 class="editorial-card-title"><a href="<?php print $teasers[$i]['article_url']; ?>"><?php print $teasers[$i]['article_title']; ?></a></h4>
+      </div>
+      <?php endfor; ?>
+    </div>
+  </div>
+  <div class="row indicator space-bottom double">
+    <div class="column">
+      <p><i class="indicator-left icon-arrow-left disabled"></i>&nbsp;&nbsp;&nbsp;<i class="indicator-right icon-arrow-right"></i></p>
+    </div>
   </div>
 </div>
-
-
