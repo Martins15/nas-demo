c37efaabb64e014fb0a794ff5d81f5d1d3c28d1e
Andrey Maximov <savagge@gmail.com>
2014-11-13 17:54:54 +0300

    [ANW-318] Fixes during review

--- drupal/sites/all/themes/custom/nas/templates/views/views-view-unformatted--nas-news--nas-more-headlines.tpl.php
+++ drupal/sites/all/themes/custom/nas/templates/views/views-view-unformatted--nas-news--nas-more-headlines.tpl.php
@@ -18,13 +18,13 @@
   <div class="editorial-card-list-item">
     <h5 class="editorial-card-title no-margin"><?php print l(t('More News »'), 'news'); ?></h5>
   </div>
-  <div class="editorial-card-banner fixed blue">
-    <div class="social-sharing">
-      <span class="social-sharing-caption small">Follow us </span>
-      <a class="social-sharing-icon white small" href="https://twitter.com/audubonsociety"><i class="icon-twitter"></i></a>
-      <a class="social-sharing-icon white small" href="https://www.facebook.com/NationalAudubonSociety"><i class="icon-facebook"></i></a>
-      <a class="social-sharing-icon white small" href="http://www.youtube.com/user/NationalAudubon"><i class="icon-youtube"></i></a>
-      <a class="social-sharing-icon white small" href="http://pinterest.com"><i class="icon-pinterest"></i></a>
-    </div>
+</div>
+<div class="editorial-card-banner fixed blue">
+  <div class="social-sharing">
+    <span class="social-sharing-caption small">Follow us </span>
+    <a class="social-sharing-icon white small" href="https://twitter.com/audubonsociety"><i class="icon-twitter"></i></a>
+    <a class="social-sharing-icon white small" href="https://www.facebook.com/NationalAudubonSociety"><i class="icon-facebook"></i></a>
+    <a class="social-sharing-icon white small" href="http://www.youtube.com/user/NationalAudubon"><i class="icon-youtube"></i></a>
+    <a class="social-sharing-icon white small" href="http://pinterest.com"><i class="icon-pinterest"></i></a>
   </div>
 </div>
