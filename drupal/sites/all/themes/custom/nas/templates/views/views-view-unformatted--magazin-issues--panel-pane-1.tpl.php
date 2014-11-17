4459d9993090bfca3af7887399d6879f4e6d1818
andriyun <andriy.yun@gmail.com>
2014-11-17 10:19:05 +0000

    [ANW-289] Theme template changes

--- drupal/sites/all/themes/custom/nas/templates/views/views-view-unformatted--magazin-issues--panel-pane-1.tpl.php
+++ drupal/sites/all/themes/custom/nas/templates/views/views-view-unformatted--magazin-issues--panel-pane-1.tpl.php
@@ -12,8 +12,10 @@
     <h2><?php print $title; ?></h2>
   </div>
 <?php endif; ?>
+<div class="magazin-issues-list">
 <?php foreach ($rows as $id => $row): ?>
   <div class="large-4 medium-6 tiny-6 columns magazine-issue<?php if ($classes_array[$id]) { print ' ' . $classes_array[$id];} ?>" >
     <?php print $row; ?>
   </div>
 <?php endforeach; ?>
+</div>
