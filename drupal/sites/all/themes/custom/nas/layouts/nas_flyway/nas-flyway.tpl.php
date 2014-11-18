6e9b42afdc68e365e6eec59552262655a90a4ef1
Andrey Maximov <savagge@gmail.com>
2014-11-18 15:21:45 +0300

    [ANW-237] Flyway layout. Removed static block 'Important Bird Areas of the Atlantic Flyway'

--- drupal/sites/all/themes/custom/nas/layouts/nas_flyway/nas-flyway.tpl.php
+++ drupal/sites/all/themes/custom/nas/layouts/nas_flyway/nas-flyway.tpl.php
@@ -85,66 +85,6 @@
     </div>
   </div>
 
-  <div class="card-set">
-    <div class="row section-header">
-      <div class="column">
-        <h2 class="thin">Conservation Projects in the Atlantic Flyway</h2>
-      </div>
-      <div class="column">
-        <ul class="section-nav inline-list">
-          <li><a href="#">More Projects »</a></li>
-        </ul>
-      </div>
-    </div>
-    <div class="row card-set-wrapper">
-      <div class="clearfix card-set-scroller">
-        <div class="tiny-4 columns">
-          <div class="editorial-card" style="min-height: 368px;">
-            <div class="editorial-card-photo">
-              <a href="#"><img src="../../img/editorial-card-10.jpg" alt=""></a>
-            </div>
-            <div class="editorial-card-content">
-              <a href="#" class="editorial-card-slug">Seas &amp; Shores</a>
-              <h4 class="editorial-card-title"><a href="#">Here’s the Name of a Conservation Project</a></h4>
-              <p>Along with the title, we should include a short description of the project. </p>
-            </div>
-          </div>
-        </div>
-        <div class="tiny-4 columns">
-          <div class="editorial-card" style="min-height: 368px;">
-            <div class="editorial-card-photo">
-              <a href="#"><img src="../../img/editorial-card-11.jpg" alt=""></a>
-            </div>
-            <div class="editorial-card-content">
-              <a href="#" class="editorial-card-slug">Working Lands</a>
-              <h4 class="editorial-card-title"><a href="#">Another Example of a Conservation Project</a></h4>
-              <p>Along with the title, we should include a short description of the project. </p>
-            </div>
-          </div>
-        </div>
-        <div class="tiny-4 columns">
-          <div class="editorial-card" style="min-height: 368px;">
-            <div class="editorial-card-photo">
-              <a href="#"><img src="../../img/editorial-card-12.jpg" alt=""></a>
-            </div>
-            <div class="editorial-card-content">
-              <a href="#" class="editorial-card-slug">Important Bird Areas</a>
-              <h4 class="editorial-card-title"><a href="#">And a THird Conservation Project Linked Here</a></h4>
-              <p>Along with the title, we should include a short description of the project. </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="row space-bottom double">
-      <div class="card-set-dots">
-        <div class="dot active"></div>
-        <div class="dot"></div>
-        <div class="dot"></div>
-      </div>
-    </div>
-  </div>
-
   <section class="card-set bg-1">
     <div class="row">
       <div class="column">
