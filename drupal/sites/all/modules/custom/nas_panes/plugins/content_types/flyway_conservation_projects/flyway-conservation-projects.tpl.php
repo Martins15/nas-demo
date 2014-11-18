e955fe9ac7107286222fdb8038a2f8f79ddbd39a
Andrey Maximov <savagge@gmail.com>
2014-11-18 18:49:15 +0300

    [ANW-238] Flyway Conservation Projects custom panels pane.

--- drupal/sites/all/modules/custom/nas_panes/plugins/content_types/flyway_conservation_projects/flyway-conservation-projects.tpl.php
+++ drupal/sites/all/modules/custom/nas_panes/plugins/content_types/flyway_conservation_projects/flyway-conservation-projects.tpl.php
@@ -15,29 +15,18 @@
   <?php print $contextual_links; ?>
   <div class="row section-header">
     <div class="column">
-      <h2 class="thin"><?php print $title; ?>Conservation Projects in the Atlantic Flyway</h2>
+      <h2 class="thin"><?php print $title; ?></h2>
     </div>
     <div class="column">
       <ul class="section-nav inline-list">
-        <li><?php print $more_link; ?> <a href="#">More Projects »</a></li>
+        <li><?php print $more_link; ?></li>
       </ul>
     </div>
   </div>
   <div class="row card-set-wrapper">
     <div class="clearfix card-set-scroller">
       <?php foreach ($teasers as $teaser): ?>
-      <div class="tiny-4 columns">
-        <div class="editorial-card" style="min-height: 368px;">
-          <div class="editorial-card-photo">
-            <a href="#"><img src="../../img/editorial-card-10.jpg" alt=""></a>
-          </div>
-          <div class="editorial-card-content">
-            <a href="#" class="editorial-card-slug">Seas &amp; Shores</a>
-            <h4 class="editorial-card-title"><a href="#">Here’s the Name of a Conservation Project</a></h4>
-            <p>Along with the title, we should include a short description of the project. </p>
-          </div>
-        </div>
-      </div>
+        <?php print $teaser; ?>
       <?php endforeach; ?>
     </div>
   </div>
