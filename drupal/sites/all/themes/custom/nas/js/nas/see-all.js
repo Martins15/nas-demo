1aa0aed3447b605eb78c04b59fa8083166f96c96
Andrey Maximov <savagge@gmail.com>
2014-11-21 21:48:31 +0300

    [ANW-297] Conservation Project page. Birds native to this area view theming

---
+++ drupal/sites/all/themes/custom/nas/js/nas/see-all.js
@@ -0,0 +1,17 @@
+/**
+ * @file Drupal behaviour 'see all' for Birds Native to This Area block.
+ */
+(function ($) {
+  Drupal.behaviors.seeall = {
+    attach: function (context, settings) {
+      $('.see-all', context).once('see-all', function () {
+        var $this = $(this);
+        $('.see-all-controller', $this).click(function () {
+          $('.see-all-hide', $this).hide();
+          $('.see-all-show', $this).show();
+          return false;
+        })
+      });
+    }
+  };
+})(jQuery);
