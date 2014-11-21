aabbf97d9dde3cda9708c5bb2c9d654eccc53794
andriyun <andriy.yun@gmail.com>
2014-11-21 10:27:40 +0000

    [ANW-289] Fix mobile tiny view

--- drupal/sites/all/themes/custom/nas/templates/views/views-view-unformatted--magazin-issues--panel-pane-1.tpl.php
+++ drupal/sites/all/themes/custom/nas/templates/views/views-view-unformatted--magazin-issues--panel-pane-1.tpl.php
@@ -14,7 +14,7 @@
 <?php endif; ?>
 <div class="magazin-issues-list">
 <?php foreach ($rows as $id => $row): ?>
-  <div class="large-4 medium-6 tiny-6 columns magazine-issue<?php if ($classes_array[$id]) { print ' ' . $classes_array[$id];} ?>" >
+  <div class="large-4 medium-6 small-6 tiny-6 columns magazine-issue<?php if ($classes_array[$id]) { print ' ' . $classes_array[$id];} ?>" >
     <?php print $row; ?>
   </div>
 <?php endforeach; ?>
