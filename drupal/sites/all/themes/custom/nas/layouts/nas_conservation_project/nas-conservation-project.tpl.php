74a150b9acf3d663dc67b99533620419edebe62a
Andrey Maximov <savagge@gmail.com>
2014-11-25 20:31:29 +0300

    [ANW-299] Conservation Project page. Explore Other Conservation Projects. Conservation projects layout updated.

--- drupal/sites/all/themes/custom/nas/layouts/nas_conservation_project/nas-conservation-project.tpl.php
+++ drupal/sites/all/themes/custom/nas/layouts/nas_conservation_project/nas-conservation-project.tpl.php
@@ -78,60 +78,6 @@
   </div>
   <?php print $content['bottom']; ?>
 
-  <section class="card-set bg-gray">
-    <div class="row space-top">
-      <div class="column">
-        <h2 class="thin">Explore Other Conservation Projects</h2>
-      </div>
-    </div>
-    <div class="row card-set-wrapper">
-      <div class="clearfix card-set-scroller">
-        <div class="tiny-4 columns">
-          <div class="editorial-card">
-            <div class="editorial-card-photo">
-              <a href="#"><img src="<?php print base_path() . drupal_get_path('theme', 'nas') . '/img/'; ?>editorial-card-10.jpg" alt=""></a>
-            </div>
-            <div class="editorial-card-content">
-              <a href="#" class="editorial-card-slug">Seas &amp; Shores</a>
-              <h3 class="editorial-card-title"><a href="#">Here’s the Name of a Conservation Project</a></h3>
-              <p>Along with the title, we should include a short description of the project. </p>
-            </div>
-          </div>
-        </div>
-        <div class="tiny-4 columns">
-          <div class="editorial-card">
-            <div class="editorial-card-photo">
-              <a href="#"><img src="<?php print base_path() . drupal_get_path('theme', 'nas') . '/img/'; ?>editorial-card-11.jpg" alt=""></a>
-            </div>
-            <div class="editorial-card-content">
-              <a href="#" class="editorial-card-slug">Seas &amp; Shores</a>
-              <h3 class="editorial-card-title"><a href="#">Another Current Seas &amp; Shores Project</a></h3>
-              <p>Along with the title, we should include a short description of the project. </p>
-            </div>
-          </div>
-        </div>
-        <div class="tiny-4 columns">
-          <div class="editorial-card">
-            <div class="editorial-card-photo">
-              <a href="#"><img src="<?php print base_path() . drupal_get_path('theme', 'nas') . '/img/'; ?>editorial-card-12.jpg" alt=""></a>
-            </div>
-            <div class="editorial-card-content">
-              <a href="#" class="editorial-card-slug">Seas &amp; Shores</a>
-              <h3 class="editorial-card-title"><a href="#">Another Current Seas &amp; Shores Project</a></h3>
-              <p>Along with the title, we should include a short description of the project. </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="row space-bottom double">
-      <div class="card-set-dots">
-        <div class="dot active"></div>
-        <div class="dot"></div>
-        <div class="dot"></div>
-      </div>
-    </div>
-  </section>
   <section class="card-set bg-1">
     <div class="row">
       <div class="column">
