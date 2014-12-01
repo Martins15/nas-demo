d882406c19ec9063d15e141327d82df8fbef15ff
atiupa <redfoxxx.ua@gmail.com>
2014-12-01 20:31:04 +0200

    [ANW-257] finnaly fixed

--- drupal/sites/all/themes/custom/nas/js/mute/expand-hero.js
+++ drupal/sites/all/themes/custom/nas/js/mute/expand-hero.js
@@ -1,27 +1,34 @@
 (function($) {
+
+  var oldWidth = 0,
+      oldHeight = 0;
+
   Drupal.behaviors.expand_hero = {
     attach: function(context, settings) {
       /*window.scrollTo(0, 0);*/
 
-      var oldWidth = $(".hero.expand").width(),
-          oldHeight = $(".hero.expand").height(),
-          oldAspectRatio = oldWidth / oldHeight;
-
-      // Assume 2:3 aspect ratio if we can't calculate it
-      if(oldHeight == 0) {
-        oldHeight = oldWidth * 0.666667;
-      }
-
       if($(".hero.expand").length > 0) {
-        expandHero();
 
         $(".hero.expand img").load(expandHero);
 
-        $(window).on("resize load", expandHero);
+        $(window).on("resize", expandHero);
       }
 
       // Expands the hero to window height for a dramatic effect
       function expandHero() {
+
+        if(oldWidth == 0) {
+          oldWidth = $(".hero.expand").width();
+        }
+        if(oldHeight == 0) {
+          oldHeight = $(".hero.expand").height();
+        }
+
+        // Assume 2:3 aspect ratio if we can't calculate it
+        if(oldHeight == 0) {
+          oldHeight = oldWidth * 0.666667;
+        }
+
         var windowHeight = $(window).height(),
             windowWidth = $(window).width(),
             aspectRatio = windowWidth / windowHeight,
@@ -33,11 +40,7 @@
           var newHeight = windowHeight - $hero.offset().top,
               newWidth = (newHeight / oldHeight) * oldWidth,
               bleed = (newWidth - windowWidth) / -2;
-          console.log('newHeight ' + newHeight);
-          console.log('newWidth ' + newWidth);
-          console.log('bleed ' + bleed);
-          console.log('oldHeight ' + oldHeight);
-          console.log('oldWidth ' + oldWidth);
+
           $hero.css({"height": newHeight + "px"});
           $img.css({
             "left": "0px",
