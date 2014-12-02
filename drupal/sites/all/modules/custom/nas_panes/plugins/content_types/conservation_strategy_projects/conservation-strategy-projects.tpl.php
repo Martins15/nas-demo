ae3abd70bdc83a1f10c828b06664a19885d89f6a
Alexander Schedrov <alexander.schedrov@gmail.com>
2014-12-02 15:36:34 +0200

    [ANW-301] Add Featured Projects block.

--- drupal/sites/all/modules/custom/nas_panes/plugins/content_types/conservation_strategy_projects/conservation-strategy-projects.tpl.php
+++ drupal/sites/all/modules/custom/nas_panes/plugins/content_types/conservation_strategy_projects/conservation-strategy-projects.tpl.php
@@ -11,43 +11,12 @@
       <h2 class="thin"><?php print $title; ?></h2>
     </div>
   </div>
-  <div class="row card-set-wrapper contextual-links-region">
+  <div class="row card-set-wrapper <?php print !empty($contextual_links) ? 'contextual-links-region' : '' ?>">
     <?php print $contextual_links; ?>
     <div class="clearfix card-set-scroller">
-      <div class="tiny-4 columns">
-        <div class="editorial-card">
-          <div class="editorial-card-photo">
-            <a href="#"><img src="/<?php print drupal_get_path('theme', 'nas') . '/img/editorial-card-10.jpg'; ?>" alt=""></a>
-          </div>
-          <div class="editorial-card-content">
-            <h3 class="editorial-card-title"><a href="#">Here’s the Name of a Conservation Project</a></h3>
-            <p>Along with the title, we should include a short description of the project. </p>
-          </div>
-        </div>
-      </div>
-      <div class="tiny-4 columns">
-        <div class="editorial-card">
-          <div class="editorial-card-photo">
-            <a href="#"><img src="/<?php print drupal_get_path('theme', 'nas') . '/img/editorial-card-11.jpg'; ?>" alt=""></a>
-          </div>
-          <div class="editorial-card-content">
-            <h3 class="editorial-card-title"><a href="#">Another Current Seas &amp; Shores Project</a></h3>
-            <p>Along with the title, we should include a short description of the project. </p>
-          </div>
-        </div>
-      </div>
-      <div class="tiny-4 columns">
-        <div class="editorial-card">
-          <div class="editorial-card-photo">
-            <a href="#"><img src="/<?php print drupal_get_path('theme', 'nas') . '/img/editorial-card-12.jpg'; ?>" alt=""></a>
-          </div>
-          <div class="editorial-card-content">
-            <h3 class="editorial-card-title"><a href="#">Another Current Seas &amp; Shores Project</a></h3>
-            <p>Along with the title, we should include a short description of the project. </p>
-          </div>
-        </div>
-      </div>
-    </div>
+      <?php foreach ($teasers as $teaser) : ?>
+        <?php print $teaser; ?>
+      <?php endforeach; ?>
   </div>
   <div class="row space-bottom double">
     <div class="card-set-dots">
