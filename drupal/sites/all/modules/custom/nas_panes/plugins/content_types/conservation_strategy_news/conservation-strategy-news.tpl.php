74135398b7faa34a4cbbac00985f90a47cc4ffd6
Alexander Schedrov <alexander.schedrov@gmail.com>
2014-12-05 14:42:49 +0200

    [ANW-302] Add relation to Conservation Project and Project Articles.

--- drupal/sites/all/modules/custom/nas_panes/plugins/content_types/conservation_strategy_news/conservation-strategy-news.tpl.php
+++ drupal/sites/all/modules/custom/nas_panes/plugins/content_types/conservation_strategy_news/conservation-strategy-news.tpl.php
@@ -6,9 +6,11 @@
 ?>
 
 <div class="row section-header space-top double">
-  <div class="column">
-    <h2 class="thin"><?php print $title; ?></h2>
-  </div>
+  <?php if (!$hide_title) : ?>
+    <div class="column">
+      <h2 class="thin"><?php print $title; ?></h2>
+    </div>
+  <?php endif; ?>
 </div>
 <div class="row <?php print !empty($contextual_links) ? 'contextual-links-region' : ''; ?>">
   <?php print $contextual_links; ?>
