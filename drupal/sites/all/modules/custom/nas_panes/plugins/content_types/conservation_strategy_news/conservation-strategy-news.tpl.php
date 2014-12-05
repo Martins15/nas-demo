efdb824877b91239a137b57f75f464913ce348e7
Alexander Schedrov <alexander.schedrov@gmail.com>
2014-12-05 16:39:44 +0200

    [ANW-302] Move conservation login to own module.

--- drupal/sites/all/modules/custom/nas_panes/plugins/content_types/conservation_strategy_news/conservation-strategy-news.tpl.php
+++ drupal/sites/all/modules/custom/nas_panes/plugins/content_types/conservation_strategy_news/conservation-strategy-news.tpl.php
@@ -5,22 +5,24 @@
  */
 ?>
 
-<div class="row section-header space-top double">
-  <?php if (!$hide_title) : ?>
-    <div class="column">
-      <h2 class="thin"><?php print $title; ?></h2>
+<?php if (!$hide_title || !empty($contextual_links)) : ?>
+  <div class="row section-header space-top double">
+    <?php if (!$hide_title) : ?>
+      <div class="column">
+        <h2 class="thin"><?php print $title; ?></h2>
+      </div>
+    <?php endif; ?>
+  </div>
+  <div class="row <?php print !empty($contextual_links) ? 'contextual-links-region' : ''; ?>">
+    <?php print $contextual_links; ?>
+    <div class="sidebar large-push-8 large-4 columns">
+      <?php foreach ($featured_news as $item) : ?>
+        <?php print $item; ?>
+      <?php endforeach; ?>
     </div>
-  <?php endif; ?>
-</div>
-<div class="row <?php print !empty($contextual_links) ? 'contextual-links-region' : ''; ?>">
-  <?php print $contextual_links; ?>
-  <div class="sidebar large-push-8 large-4 columns">
-    <?php foreach ($featured_news as $item) : ?>
-      <?php print $item; ?>
-    <?php endforeach; ?>
-  </div>
 
-  <div class="large-8 large-pull-4 columns index-list">
-    <?php print $news_list; ?>
-  </div>
+    <div class="large-8 large-pull-4 columns index-list">
+      <?php print $news_list; ?>
+    </div>
 </div>
+<?php endif; ?>
