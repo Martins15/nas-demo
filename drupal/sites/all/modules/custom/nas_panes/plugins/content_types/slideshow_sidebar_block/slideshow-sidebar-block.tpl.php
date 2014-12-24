2c7dfda3e184b94b41826ef0d0894f316e222b0f
Alexander Schedrov <alexander.schedrov@gmail.com>
2014-12-24 15:52:50 +0200

    [ANW-392] Add Slideshow sidebar block.

---
+++ drupal/sites/all/modules/custom/nas_panes/plugins/content_types/slideshow_sidebar_block/slideshow-sidebar-block.tpl.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @file
+ * Slideshow Sidebad Block template file.
+ */
+?>
+
+<section class="sidebar-section medium-6 large-12 columns">
+  <div class="sidebar-section editorial-card slideshow-sidebar-block <?php print !empty($contextual_links) ? 'contextual-links-region' : ''; ?>">
+    <?php print $contextual_links; ?>
+
+    <div class="editorial-card-photo">
+      <?php print $image; ?>
+    </div>
+
+    <div class="editorial-card-banner gray"><i class="icon-slides"></i> <?php print $block_title; ?></div>
+    <div class="editorial-card-content">
+      <h3 class="editorial-card-title"><?php print $slideshow; ?></h3>
+    </div>
+  </div>
+</section>
