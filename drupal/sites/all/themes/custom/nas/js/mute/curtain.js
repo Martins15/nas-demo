2b35225d89bc138a83ad049d3dd9ca0d5644f782
atiupa <redfoxxx.ua@gmail.com>
2014-12-25 16:15:47 +0200

    minor fixes

--- drupal/sites/all/themes/custom/nas/js/mute/curtain.js
+++ drupal/sites/all/themes/custom/nas/js/mute/curtain.js
@@ -1,73 +1,77 @@
 (function($) {
-  function Curtain($curtain) {
-    var self = this,
-        $el = $curtain,
-        $body = $("body"),
-        $wrapper = $(".curtain-wrapper");
+  Drupal.behaviors.curtain = {
+    attach: function (context, settings) {
+      function Curtain($curtain) {
+        var self = this,
+            $el = $curtain,
+            $body = $("body"),
+            $wrapper = $(".curtain-wrapper");
 
-    // Tracking curtain movement and status
-    self.height = 0;
-    self.buffer = 200;
+        // Tracking curtain movement and status
+        self.height = 0;
+        self.buffer = 200;
 
-    self.init = function() {
-      window.scrollTo(0, 1);
+        self.init = function() {
+          window.scrollTo(0, 1);
 
-      // Make sure the body's got height while everything's fixed
-      $body.css({"min-height": $body.height()});
+          // Make sure the body's got height while everything's fixed
+          $body.css({"min-height": $body.height()});
 
-      self.setCurtainFocus(true);
-      self.bind();
-    }
+          self.setCurtainFocus(true);
+          self.bind();
+        }
 
-    self.bind = function() {
-      $(window).on("scroll", self.handleScroll);
-    }
+        self.bind = function() {
+          $(window).on("scroll", self.handleScroll);
+        }
 
-    self.handleScroll = function(e) {
-      var scrollFactor = document.documentElement.scrollTop || $(document).scrollTop();
+        self.handleScroll = function(e) {
+          var scrollFactor = document.documentElement.scrollTop || $(document).scrollTop();
 
-      // If we're scrolled down past the curtain, let the page scroll
-      if(scrollFactor > self.getTotalHeight()) {
-        self.setCurtainFocus(true);
+          // If we're scrolled down past the curtain, let the page scroll
+          if(scrollFactor > self.getTotalHeight()) {
+            self.setCurtainFocus(true);
+          }
+          // And if we're not, make sure the curtain is all that scrolls
+          else {
+            self.setCurtainFocus(false);
+          }
+        }
+
+        self.setCurtainFocus = function(state) {
+          self.updateHeight();
+
+          if(state) {
+            $wrapper.attr("style", "margin-top: " + self.getTotalHeight() + "px");
+            $wrapper.removeClass("on");
+          } 
+          else {
+            $wrapper.attr("style", "margin-bottom: " + self.getTotalHeight() + "px");
+            $wrapper.addClass("on");
+          }
+        }
+
+        // Update the height of the curtain, in case it's changed
+        self.updateHeight = function() {
+          self.height = $curtain.height();
+          return self.height;
+        }
+
+        // Curtain height plus an arbitrary buffer
+        self.getTotalHeight = function() {
+          return self.height + self.buffer;
+        }
+
+        self.init();
       }
-      // And if we're not, make sure the curtain is all that scrolls
-      else {
-        self.setCurtainFocus(false);
-      }
+
+      $(function() {
+        var $curtain = $(".curtain");
+
+        if($curtain.length) {
+          Curtain($curtain);
+        }
+      });
     }
-
-    self.setCurtainFocus = function(state) {
-      self.updateHeight();
-
-      if(state) {
-        $wrapper.attr("style", "margin-top: " + self.getTotalHeight() + "px");
-        $wrapper.removeClass("on");
-      } 
-      else {
-        $wrapper.attr("style", "margin-bottom: " + self.getTotalHeight() + "px");
-        $wrapper.addClass("on");
-      }
-    }
-
-    // Update the height of the curtain, in case it's changed
-    self.updateHeight = function() {
-      self.height = $curtain.height();
-      return self.height;
-    }
-
-    // Curtain height plus an arbitrary buffer
-    self.getTotalHeight = function() {
-      return self.height + self.buffer;
-    }
-
-    self.init();
-  }
-
-  $(function() {
-    var $curtain = $(".curtain");
-
-    if($curtain.length) {
-      Curtain($curtain);
-    }
-  });
+  };
 })(jQuery);
--- drupal/sites/all/themes/custom/nas/js/mute/wrap-select.js
+++ drupal/sites/all/themes/custom/nas/js/mute/wrap-select.js
@@ -1,8 +1,12 @@
-$(function() {
-  if(navigator.userAgent.match("Firefox")) {
-    $("select").wrap("<div class='-wrap-select'></div>");
-  }
-  else if(navigator.userAgent.match("MSIE 8.") || navigator.userAgent.match("MSIE 9.")) {
-    $("select").wrap("<div class='-wrap-select ie'></div>");
-  }
-});
+(function($) {
+  Drupal.behaviors.wrap_select = {
+    attach: function (context, settings) {
+      if(navigator.userAgent.match("Firefox")) {
+        $("select").wrap("<div class='-wrap-select'></div>");
+      }
+      else if(navigator.userAgent.match("MSIE 8.") || navigator.userAgent.match("MSIE 9.")) {
+        $("select").wrap("<div class='-wrap-select ie'></div>");
+      }
+    }
+  };
+})(jQuery);
