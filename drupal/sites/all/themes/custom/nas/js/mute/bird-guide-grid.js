35d101928fe834027d67f4e679197d27a2325d2a
atiupa <redfoxxx.ua@gmail.com>
2014-12-26 18:32:14 +0200

    [ANW-400] implement BOA plate binoculars functionality

--- drupal/sites/all/themes/custom/nas/js/mute/bird-guide-grid.js
+++ drupal/sites/all/themes/custom/nas/js/mute/bird-guide-grid.js
@@ -1,15 +1,21 @@
 (function($) {
+  Drupal.behaviors.bird_guide_grid = {
+    attach: function (context, settings) {
+      $(".bg-egg").bind("click", function(e) {
+        e.preventDefault();
 
-$(function() {
-  $(".bg-egg").bind("click", function(e) {
-    e.preventDefault();
+        var $this = $(this),
+            $parent = $this.parent();
 
-    var $this = $(this),
-        $parent = $this.parent();
-
-    $this.toggleClass("active").toggleClass("icon-close");
-    $parent.toggleClass("show-bg");
-  });
-});
-
+        $this.toggleClass("active").toggleClass("icon-close");
+        $parent.toggleClass("show-bg");
+        if ($parent.hasClass("show-bg")) {
+          $parent.css('background-image', 'url(' + settings.boa_plate_binocular.image_color + ')');
+        }
+        else {
+          $parent.css('background-image', 'url(' + settings.boa_plate_binocular.image_desaturated + ')');
+        }
+      });
+    }
+  };
 })(jQuery);
