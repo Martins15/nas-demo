f45da2eaa3f68c7a8fd2670e8a2a5be1f8ef1852
atiupa <redfoxxx.ua@gmail.com>
2014-12-29 12:14:41 +0200

    [ANW-400] add some fixes for non-illustration behaviour

--- drupal/sites/all/modules/custom/nas_panes/plugins/content_types/boa_plate_binocular/boa-plate-binocular.tpl.php
+++ drupal/sites/all/modules/custom/nas_panes/plugins/content_types/boa_plate_binocular/boa-plate-binocular.tpl.php
@@ -8,10 +8,12 @@
  * - $title: linked title of BOA.
  * - $credits: Illustration credits.
  * - $link: link to BOA node.
+ * - $image_desaturated: image from field_boa_plate_illustr_des.
  */
 ?>
 <div class="<?php print $class; ?> contextual-links-region" style="background-image: url(<?php print $image_desaturated; ?>);">
   <?php print $contextual_links; ?>
+  <?php if (!empty($title)): ?>
   <a href="#" class="bg-egg icon-binoculars white"></a>
   <div class="bird-card-grid-bg-caption">
     <div class="row">
@@ -24,3 +26,4 @@
       </div>
     </div>
   </div>
+  <?php endif; ?>
