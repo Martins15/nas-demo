cd825cdf16c4cbbe1047448781a75524e279dede
Andrey Maximov <savagge@gmail.com>
2014-12-29 15:31:38 +0300

    [ANW-404] Search form.

---
+++ drupal/sites/all/modules/custom/nas_search/templates/nas_search_page_search_form.tpl.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: savage
+ * Date: 28.12.14
+ * Time: 14:14
+ */
+
+?>
+
+<div class="top-search-form-input">
+  <?php print render($form['search']); ?>
+</div>
+<?php print render($form['submit']); ?>
+<?php //print drupal_render_children($form); ?>
