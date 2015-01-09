fd8d896aa5c2099d03f64bf8f3c526eba7af2511
Andrey Maximov <savagge@gmail.com>
2015-01-09 17:11:52 +0300

    [ANW-71] Donate panes introduced

---
+++ drupal/sites/all/modules/custom/nas_panes/plugins/content_types/donate_cards_3x/donate-cards-3x.tpl.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @file
+ * Template to custom pane Donate cards (3x).
+ *
+ * Available variables:
+ *  $contextual_links - contextual links.
+ *  $additional_classes - additional html classes for wrapper.
+ *  $spacetop - flag indicating to add additional top padding.
+ *  $title - pane's title.
+ *  $teasers - array of rendered news.
+ */
+?>
+
+<?php if ($teasers || $contextual_links): ?>
+<div class="card-set editorial-card-3x <?php print $additional_classes; ?> contextual-links-region">
+  <?php print $contextual_links; ?>
+  <?php if ($title): ?>
+    <div class="row section-header<?php if (!empty($spacetop)): ?> space-top<?php endif; ?>">
+      <?php if ($title): ?>
+      <div class="column">
+        <h2 class="thin"><?php print $title; ?></h2>
+      </div>
+      <?php endif; ?>
+    </div>
+  <?php endif; ?>
+
+  <div class="row space-bottom double">
+    <?php foreach ($teasers as $teaser): ?>
+      <?php if (is_string($teaser)): ?>
+        <?php print $teaser; ?>
+      <?php elseif (is_array($teaser)): ?>
+        <div class="columns large-4">
+          <div class="editorial-card<?php if (!$teaser['linked_image']): ?> editorial-card-no-image<?php endif; ?> collapse-minimal">
+            <div class="editorial-card-photo">
+              <?php print $teaser['linked_image']; ?>
+            </div>
+            <div class="editorial-card-content short">
+              <h4 class="editorial-card-title"><?php print $teaser['title']; ?></h4>
+              <?php if (!empty($teaser['excerpt'])): ?>
+                <p><?php print $teaser['excerpt']; ?></p>
+              <?php endif; ?>
+            </div>
+          </div>
+        </div>
+      <?php endif; ?>
+    <?php endforeach; ?>
+  </div>
+</div>
+<?php endif; ?>
