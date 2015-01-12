af9f75b44b8f2129366363963d81c8ec17f275f1
Andrey Maximov <savagge@gmail.com>
2015-01-12 19:10:31 +0300

    [ANW-404] Wrap message with t().

--- drupal/sites/all/modules/custom/nas_panes/plugins/content_types/search_results_count/search-results-empty.tpl.php
+++ drupal/sites/all/modules/custom/nas_panes/plugins/content_types/search_results_count/search-results-empty.tpl.php
@@ -8,6 +8,6 @@
 <div class="row space-top">
   <div class="columns">
     <h2>Articles</h2>
-    <p class="empty-search-results">No content was found to match your search. Try modifying your search criteria and try again.</p>
+    <p class="empty-search-results"><?php print $message; ?></p>
   </div>
 </div>
