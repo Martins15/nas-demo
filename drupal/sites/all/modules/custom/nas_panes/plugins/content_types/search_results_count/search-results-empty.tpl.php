fb8a7cecb6d5d7512c138ef53d0ac3ac6217f341
Andrey Maximov <savagge@gmail.com>
2015-01-12 18:08:08 +0300

    [ANW-404] Search results page: empty results pane refinement

---
+++ drupal/sites/all/modules/custom/nas_panes/plugins/content_types/search_results_count/search-results-empty.tpl.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @file
+ * Search page editorial results count pane template.
+ */
+?>
+
+<div class="row space-top">
+  <div class="columns">
+    <h2>Articles</h2>
+    <p class="empty-search-results">No content was found to match your search. Try modifying your search criteria and try again.</p>
+  </div>
+</div>
