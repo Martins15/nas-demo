f01d0d168d1f57a33081c5c901e01ad0c6be13e7
Andrey Maximov <savagge@gmail.com>
2015-01-13 19:44:00 +0300

    [ANW-71] Make 3x cardsets to equalize cards by lines.

--- docroot/sites/all/modules/custom/nas_panes/plugins/content_types/donate_cards_3x/donate-cards-3x.tpl.php
+++ docroot/sites/all/modules/custom/nas_panes/plugins/content_types/donate_cards_3x/donate-cards-3x.tpl.php
@@ -13,7 +13,7 @@
 ?>
 
 <?php if ($teasers || $contextual_links): ?>
-<div class="card-set editorial-card-3x <?php print $additional_classes; ?> contextual-links-region">
+<div class="editorial-card-3x <?php print $additional_classes; ?> contextual-links-region">
   <?php print $contextual_links; ?>
   <?php if ($title): ?>
     <div class="row section-header<?php if (!empty($spacetop)): ?> space-top<?php endif; ?><?php if (!empty($spacebetween)): ?> space-bottom<?php endif; ?>">
@@ -26,24 +26,28 @@
   <?php endif; ?>
 
   <div class="row <?php print !empty($spacebottom) ? $spacebottom : ''; ?>">
-    <?php foreach ($teasers as $teaser): ?>
-      <?php if (is_string($teaser)): ?>
-        <?php print $teaser; ?>
-      <?php elseif (is_array($teaser)): ?>
-        <div class="columns large-4">
-          <div class="editorial-card<?php if (!$teaser['linked_image']): ?> editorial-card-no-image<?php endif; ?> collapse-minimal">
-            <div class="editorial-card-photo">
-              <?php print $teaser['linked_image']; ?>
-            </div>
-            <div class="editorial-card-content short">
-              <h4 class="editorial-card-title"><?php print $teaser['title']; ?></h4>
-              <?php if (!empty($teaser['excerpt'])): ?>
-                <p><?php print $teaser['excerpt']; ?></p>
-              <?php endif; ?>
+    <?php foreach ($chunks as $teasers): ?>
+      <div class="row column card-set space-bottom">
+      <?php foreach ($teasers as $teaser): ?>
+        <?php if (is_string($teaser)): ?>
+          <?php print $teaser; ?>
+        <?php elseif (is_array($teaser)): ?>
+          <div class="columns large-4">
+            <div class="editorial-card<?php if (!$teaser['linked_image']): ?> editorial-card-no-image<?php endif; ?> collapse-minimal">
+              <div class="editorial-card-photo">
+                <?php print $teaser['linked_image']; ?>
+              </div>
+              <div class="editorial-card-content short">
+                <h4 class="editorial-card-title"><?php print $teaser['title']; ?></h4>
+                <?php if (!empty($teaser['excerpt'])): ?>
+                  <p><?php print $teaser['excerpt']; ?></p>
+                <?php endif; ?>
+              </div>
             </div>
           </div>
-        </div>
-      <?php endif; ?>
+        <?php endif; ?>
+      <?php endforeach; ?>
+      </div>
     <?php endforeach; ?>
   </div>
 </div>
--- docroot/sites/all/modules/custom/nas_panes/plugins/content_types/editorial_card_3x/editorial-card-3x.tpl.php
+++ docroot/sites/all/modules/custom/nas_panes/plugins/content_types/editorial_card_3x/editorial-card-3x.tpl.php
@@ -14,7 +14,7 @@
 ?>
 
 <?php if ($teasers || $contextual_links): ?>
-<div class="card-set editorial-card-3x <?php print $additional_classes; ?> contextual-links-region">
+<div class="editorial-card-3x <?php print $additional_classes; ?> contextual-links-region">
   <?php print $contextual_links; ?>
   <?php if ($title || $more_link): ?>
     <div class="row section-header<?php if (!empty($spacetop)): ?> space-top<?php endif; ?><?php if (!empty($spacebetween)): ?> space-bottom<?php endif; ?>">
@@ -34,26 +34,30 @@
   <?php endif; ?>
 
   <div class="row <?php print !empty($spacebottom) ? $spacebottom : ''; ?>">
-    <?php foreach ($teasers as $teaser): ?>
-      <?php if (is_string($teaser)): ?>
-        <?php print $teaser; ?>
-      <?php elseif (is_array($teaser)): ?>
-        <div class="columns large-4">
-          <div class="editorial-card<?php if (!$teaser['linked_image']): ?> editorial-card-no-image<?php endif; ?> collapse-minimal">
-            <div class="editorial-card-photo">
-              <?php print $teaser['linked_image']; ?>
-            </div>
-            <div class="editorial-card-content short">
-              <?php print $teaser['category_link']; ?>
-              <h4 class="editorial-card-title"><?php print $teaser['title']; ?></h4>
-              <?php if (!empty($teaser['excerpt'])): ?>
-                <p><?php print $teaser['excerpt']; ?></p>
-              <?php endif; ?>
-              <p><em><?php print $teaser['readmore_link']; ?></em></p>
+    <?php foreach ($chunks as $teasers): ?>
+      <div class="row column card-set space-bottom">
+      <?php foreach ($teasers as $teaser): ?>
+        <?php if (is_string($teaser)): ?>
+          <?php print $teaser; ?>
+        <?php elseif (is_array($teaser)): ?>
+          <div class="columns large-4">
+            <div class="editorial-card<?php if (!$teaser['linked_image']): ?> editorial-card-no-image<?php endif; ?> collapse-minimal">
+              <div class="editorial-card-photo">
+                <?php print $teaser['linked_image']; ?>
+              </div>
+              <div class="editorial-card-content short">
+                <?php print $teaser['category_link']; ?>
+                <h4 class="editorial-card-title"><?php print $teaser['title']; ?></h4>
+                <?php if (!empty($teaser['excerpt'])): ?>
+                  <p><?php print $teaser['excerpt']; ?></p>
+                <?php endif; ?>
+                <p><em><?php print $teaser['readmore_link']; ?></em></p>
+              </div>
             </div>
           </div>
-        </div>
-      <?php endif; ?>
+        <?php endif; ?>
+      <?php endforeach; ?>
+      </div>
     <?php endforeach; ?>
   </div>
 </div>
