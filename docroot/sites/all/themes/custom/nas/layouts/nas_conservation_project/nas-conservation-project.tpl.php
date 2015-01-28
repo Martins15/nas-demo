b05059888e04d38f1e297aec6b16ed9158b6f5a1
atiupa <redfoxxx.ua@gmail.com>
2015-01-28 21:11:02 +0200

    [ANW-516] replace static share links by real one

--- docroot/sites/all/themes/custom/nas/layouts/nas_conservation_project/nas-conservation-project.tpl.php
+++ docroot/sites/all/themes/custom/nas/layouts/nas_conservation_project/nas-conservation-project.tpl.php
@@ -54,13 +54,5 @@
 
   <section class="card-set bg-1">
     <?php print $content['cards_set']; ?>
-    <div class="row">
-      <div class="card-set-social social-sharing">
-        <span class="social-sharing-caption white">Spread the word. It&rsquo;s the least you can do.</span>
-        <a class="social-sharing-icon white" href="#"><i class="icon-twitter"></i></a>
-        <a class="social-sharing-icon white" href="#"><i class="icon-facebook"></i></a>
-        <a class="social-sharing-icon white" href="#"><i class="icon-mail"></i></a>
-      </div>
-    </div>
   </section>
 </section>
