5297c4a17f83d05e6269d46501c2811e1c9b6231
Andrey Maximov <savagge@gmail.com>
2015-02-05 16:55:37 +0300

    [ANW-481] Refine twitter share links

--- docroot/sites/all/themes/custom/nas/template.php
+++ docroot/sites/all/themes/custom/nas/template.php
@@ -118,8 +118,11 @@ function nas_preprocess_node_bird(&$vars) {
   // Add Page absolute url.
   $vars['page_link'] = $base_url . '/' . drupal_get_path_alias();
   // Add Twitter share url.
-  $twitter_text = drupal_get_title() . ' ' . $vars['page_link'] . ' via @audubonsociety';
-  $vars['twitter_url'] = url('http://twitter.com/share', array('query' => array('text' => $twitter_text)));
+  $vars['twitter_url'] = url('http://twitter.com/share', array('query' => array(
+    'text' => drupal_get_title(),
+    'via' => 'audubonsociety',
+    'url' => $vars['page_link'],
+  )));
   // Add learn mode link.
   $vars['learn_more_node_link'] = l(t('Learn more »'), 'node/' . $vars['nid']);
 
@@ -1218,8 +1221,11 @@ function nas_preprocess_nas_conservation_project(&$vars) {
   // Add Page title.
   $vars['page_title'] = drupal_get_title();
 
-  $twitter_text = $vars['page_title'] . ' ' . $vars['page_link'] . ' via @audubonsociety';
-  $vars['twitter_url'] = url('http://twitter.com/share', array('query' => array('text' => $twitter_text)));
+  $vars['twitter_url'] = url('http://twitter.com/share', array('query' => array(
+    'text' => $vars['page_title'],
+    'via' => 'audubonsociety',
+    'url' => $vars['page_link'],
+  )));
 }
 
 /**
