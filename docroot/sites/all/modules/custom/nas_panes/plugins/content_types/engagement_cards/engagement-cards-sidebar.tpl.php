ca2c9a828ed4eea6f78eb2c14dca73345c5315c2
Andrey Maximov <savagge@gmail.com>
2015-02-06 22:26:33 +0300

    [ANW-553] Improve sidebar engagement card pane

--- docroot/sites/all/modules/custom/nas_panes/plugins/content_types/engagement_cards/engagement-cards-sidebar.tpl.php
+++ docroot/sites/all/modules/custom/nas_panes/plugins/content_types/engagement_cards/engagement-cards-sidebar.tpl.php
@@ -8,12 +8,22 @@
  *  $teaser - rendered node.
  */
 ?>
-<div class="sidebar-section engagement-card-sidebar-section <?php print $ftv_class; ?> contextual-links-region">
+<div class="sidebar-section engagement-card-sidebar-section <?php print $ftv_class; ?> contextual-links-region" <?php print $equalize_height; ?>>
   <?php print $contextual_links; ?>
   <?php foreach ($teasers as $teaser): ?>
     <?php print $teaser; ?>
   <?php endforeach; ?>
   <?php if (empty($teasers)): ?>
     <p>&nbsp;</p>
+  <?php endif; ?>
+  <?php if ($show_social_icons): ?>
+    <div class="editorial-card-banner editorial-card-banner-frontpage blue">
+      <div class="social-sharing">
+        <span class="social-sharing-caption small">Follow us </span>
+        <a class="social-sharing-icon white small" href="<?php print $social_icons['twitter']; ?>"><i class="icon-twitter"></i></a>
+        <a class="social-sharing-icon white small" href="<?php print $social_icons['facebook']; ?>"><i class="icon-facebook"></i></a>
+        <a class="social-sharing-icon white small" href="<?php print $social_icons['instagram']; ?>"><i class="icon-instagram"></i></a>
+      </div>
+    </div>
   <?php endif; ?>
 </div>
