050643c142c4fde44bbcb3aceca24e49ac17e27f
Yuriy Gerasimov <yuri.gerasimov@gmail.com>
2015-03-17 13:05:23 +0200

    Switch to ansible based vagrant box.

---
+++ puphpet/ansible/run-ansible-playbook.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo 'Install apache'
+ansible-playbook /vagrant/puphpet/ansible/apache.yml
+echo 'Finished installing apache'
+
+echo 'Install php'
+ansible-playbook /vagrant/puphpet/ansible/php.yml
+echo 'Finished installing php'
+
+echo 'Install php xdebug'
+ansible-playbook /vagrant/puphpet/ansible/php-xdebug.yml
+echo 'Finished installing php xdebug'
+
+echo 'Install mysql'
+ansible-playbook /vagrant/puphpet/ansible/mysql.yml
+echo 'Finished installing mysql'
+
+echo 'Install memcached'
+ansible-playbook /vagrant/puphpet/ansible/memcached.yml
+echo 'Finished installing memcached'
+
+echo 'Install composer'
+ansible-playbook /vagrant/puphpet/ansible/composer.yml
+echo 'Finished installing composer'
+
+echo 'Install drush'
+ansible-playbook /vagrant/puphpet/ansible/drush.yml
+echo 'Finished installing drush'
+
+echo 'Install Apache Solr'
+ansible-playbook /vagrant/puphpet/ansible/solr.yml
+echo 'Finished installing Apache Solr'
