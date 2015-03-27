545028e59a6a970459328eb0e7a9918f2c317782
atiupa <redfoxxx.ua@gmail.com>
2015-03-27 12:33:44 +0200

    [ANW-630] some improvements

--- docroot/sites/all/themes/custom/nas/templates/views/views-view-unformatted--nas-bird-guide--nas-bird-guide-fav-birds.tpl.php
+++ docroot/sites/all/themes/custom/nas/templates/views/views-view-unformatted--nas-bird-guide--nas-bird-guide-fav-birds.tpl.php
@@ -1,5 +1,5 @@
 <?php
-
+//dpm();
 /**
  * @file
  * Custom view template to display Explore Similar Birds.
@@ -15,7 +15,7 @@
 </div>
 <div class="view-content row space-bottom double bird-card-grid-container">
   <?php foreach ($rows as $id => $row): ?>
-    <div class="views-row columns tiny-6 medium-4 large-3">
+    <div class="page-<?php print $view->query->pager->current_page; ?> views-row columns tiny-6 medium-4 large-3">
     <?php print $row; ?>
     </div>
   <?php endforeach; ?>
