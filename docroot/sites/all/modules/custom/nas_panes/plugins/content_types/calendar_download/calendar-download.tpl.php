0868edfd4e2f28fe186bf9028971d5f54542b861
Renan Dias <renan@wearepropeople.com.br>
2015-04-06 18:07:47 -0300

    ANW641 Add Event to calendar

---
+++ docroot/sites/all/modules/custom/nas_panes/plugins/content_types/calendar_download/calendar-download.tpl.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file
+ * Calendar download template file.
+ */
+?>
+
+<div class="row">
+  <div class="column">
+    <h2 class="thin">
+      <?php print $title; ?>
+    </h2>
+  </div>
+</div>
+<div class="row text-container">
+  <div class="columns">
+   <?php echo $download_link; ?>
+  </div>
+</div>
