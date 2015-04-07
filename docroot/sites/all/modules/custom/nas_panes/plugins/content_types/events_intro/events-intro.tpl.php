d343ce1b984a8b45b0d4191a49071524e922b097
atiupa <redfoxxx.ua@gmail.com>
2015-04-07 12:34:50 +0300

    [ANW-62] some fixes

--- docroot/sites/all/modules/custom/nas_panes/plugins/content_types/events_intro/events-intro.tpl.php
+++ docroot/sites/all/modules/custom/nas_panes/plugins/content_types/events_intro/events-intro.tpl.php
@@ -5,7 +5,9 @@
  */
 ?>
 <p class="deck"><?php print $date_city_province; ?></p>
+<?php if (!empty($address)): ?>
 <div class="location-details">
   <h4><?php print t('Location Details'); ?></h4>
   <p><?php print $address; ?></p>
 </div>
+<?php endif; ?>
