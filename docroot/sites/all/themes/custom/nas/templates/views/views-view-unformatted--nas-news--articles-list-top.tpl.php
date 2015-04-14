81ef646c40b37e20a75eae534af0b0e4f403dc17
atiupa <redfoxxx.ua@gmail.com>
2015-04-14 17:56:29 +0300

    [NAS-17] Change logic of displaying News page Donate bar

--- docroot/sites/all/themes/custom/nas/templates/views/views-view-unformatted--nas-news--articles-list-top.tpl.php
+++ docroot/sites/all/themes/custom/nas/templates/views/views-view-unformatted--nas-news--articles-list-top.tpl.php
@@ -1,11 +1,13 @@
 <?php
-
 /**
  * @file
  * Default simple view template to display a list of rows.
  *
  * @ingroup views_templates
  */
+
+  // Get rendered Donate pane and place it after 6th news.
+  $show_news_donate = &drupal_static('show_news_donate');
 ?>
 <?php if (!empty($title)): ?>
   <h3><?php print $title; ?></h3>
@@ -14,4 +16,7 @@
   <div<?php if ($classes_array[$id]) { print ' class="page-' . $view->query->pager->current_page . ' ' . $classes_array[$id] .'"';  } ?>>
     <?php print $row; ?>
   </div>
+  <?php if ($id == 5 && !empty($show_news_donate)): ?>
+    <?php print $show_news_donate; ?>
+  <?php endif; ?>
 <?php endforeach; ?>
