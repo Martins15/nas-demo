eeb5e6da928a30e3e087c24ae24bd1596a2c60a0
Andrey Maximov <savagge@gmail.com>
2015-04-16 17:27:21 +0300

    [ANW-653] Allow usage of different layouts for frontpage

---
+++ docroot/sites/all/themes/custom/nas/layouts/nas_frontpage/panels-nas-frontpage--hero-image.tpl.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @file
+ * Template implementation to display the panel's layout.
+ */
+?>
+<header class="global-header <?php print drupal_static('nas_header_class'); ?>">
+  <?php print $content['header']; ?>
+</header>
+<?php print $content['featured_top']; ?>
+<section class="global-content">
+  <?php if (!empty($featured_frontpage_mobile_content)): ?>
+    <?php print $featured_frontpage_mobile_content; ?>
+  <?php endif; ?>
+  <div class="homepage-first-row row space-top double" data-equalizer>
+    <?php print $content['featured']; ?>
+    <div class="columns large-4 medium-6">
+      <?php print $content['more_headlines']; ?>
+    </div>
+  </div>
+  <?php print $content['editorial_cards']; ?>
+  <?php print $content['conservation_section']; ?>
+  <?php print $content['bird_news']; ?>
+  <?php print $content['main']; ?>
+  <section class="card-set bg-1">
+    <?php print $content['cards_set']; ?>
+  </section>
+</section>
+<?php print $content['footer']; ?>
