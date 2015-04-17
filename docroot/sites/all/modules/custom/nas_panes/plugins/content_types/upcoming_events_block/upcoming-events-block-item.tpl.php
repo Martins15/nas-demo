ac779712f74a4cd9291744e4205700f6beef8344
Renan Dias <renan@wearepropeople.com.br>
2015-04-16 21:13:09 -0300

    [ANW-650] Final Ajax fixes

--- docroot/sites/all/modules/custom/nas_panes/plugins/content_types/upcoming_events_block/upcoming-events-block-item.tpl.php
+++ docroot/sites/all/modules/custom/nas_panes/plugins/content_types/upcoming_events_block/upcoming-events-block-item.tpl.php
@@ -6,13 +6,15 @@
 ?>
 <div id="upcoming-events-content-wrapper">
   <ul class="no-bullets item-margin">
-    <?php foreach($locations as $location) : ?>
-      <li>
-		<small><?php print $location['time']; ?></small>
-        <h5 class="editorial-card-title blue close-heading"><?php print $location['link']; ?></h5>
-        <small><?php print $location['city'] . ', ' . $location['state']; ?></small>
-        <?php if ($location['order'] == 0) print '<hr>'; ?>
-      </li>
-    <?php endforeach; ?>
+	<?php if (is_array($locations)): ?>
+      <?php foreach($locations as $location) : ?>
+        <li>
+		  <?php if ($location['order'] == 1) print '<hr>'; ?>
+		  <small><?php print $location['time']; ?></small>
+          <h5 class="editorial-card-title blue close-heading"><?php print $location['link']; ?></h5>
+          <small><?php print $location['city'] . ', ' . $location['state']; ?></small>
+        </li>
+      <?php endforeach; ?>
+    <?php endif; ?>
   </ul>
 </div>
