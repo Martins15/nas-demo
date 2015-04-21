6f187da4ff146f9daf209fd9ed4e200a5daaf0cb
Andrii TIupa <andrii.tiupa@wearepropeople.com>
2015-04-21 21:07:04 +0300

    fix

--- docroot/sites/all/modules/custom/nas_panes/plugins/content_types/events_intro/events-intro.tpl.php
+++ docroot/sites/all/modules/custom/nas_panes/plugins/content_types/events_intro/events-intro.tpl.php
@@ -5,12 +5,12 @@
  */
 ?>
 <p class="deck"><?php print $date_city_province; ?></p>
-<?php if (!empty($address)): ?>
 <?php if (!empty($featured_image)): ?>
 <div class="featured-image">
   <?php print $featured_image; ?>
 </div>
 <?php endif; ?>
+<?php if (!empty($address)): ?>
 <div class="location-details">
   <h4><?php print t('Location Details'); ?></h4>
   <p><?php print $address; ?></p>
