110df20b6dbe4e32aea0e5c57fbae9a45be8790e
Andrii Tiupa <redfox@Andriis-MacBook-Pro.local>
2015-04-21 20:09:17 +0300

    [ANW-657] Output Featured image

--- docroot/sites/all/modules/custom/nas_panes/plugins/content_types/events_intro/events-intro.tpl.php
+++ docroot/sites/all/modules/custom/nas_panes/plugins/content_types/events_intro/events-intro.tpl.php
@@ -6,6 +6,11 @@
 ?>
 <p class="deck"><?php print $date_city_province; ?></p>
 <?php if (!empty($address)): ?>
+<?php if (!empty($featured_image)): ?>
+<div class="featured-image">
+	<?php print $featured_image; ?>
+</div>
+<?php endif; ?>
 <div class="location-details">
   <h4><?php print t('Location Details'); ?></h4>
   <p><?php print $address; ?></p>
