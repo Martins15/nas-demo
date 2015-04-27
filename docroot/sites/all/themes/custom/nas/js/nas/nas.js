e91ce86047a5a920a4900e1c6b54e4887c9f459e
Renan Dias <renan@wearepropeople.com.br>
2015-04-27 14:43:15 -0300

    [NAS-11] Add JS style

--- docroot/sites/all/themes/custom/nas/js/nas/nas.js
+++ docroot/sites/all/themes/custom/nas/js/nas/nas.js
@@ -402,6 +402,8 @@ var Nas = Nas || {};
             $(highlight[i]).each(highlight_aplly);
           }
         }
+        $('.highlight').css('background-color', 'yellow');
+        $('.highlight').css('color', 'black');
       });
     }
   };
