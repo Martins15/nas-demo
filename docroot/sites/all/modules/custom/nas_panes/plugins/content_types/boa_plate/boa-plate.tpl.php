ad3f8610da31e21719143d36a6cd876633c0055e
Andrii TIupa <andrii.tiupa@wearepropeople.com>
2015-04-28 18:49:10 +0300

    [ANW-660] add logic for Birds of America High Resolution Image Download

--- docroot/sites/all/modules/custom/nas_panes/plugins/content_types/boa_plate/boa-plate.tpl.php
+++ docroot/sites/all/modules/custom/nas_panes/plugins/content_types/boa_plate/boa-plate.tpl.php
@@ -8,12 +8,20 @@
  *  $image - url to the illustrtion image.
  *  $link_url - url to Learn more about John J Audubon.
  *  $link_title - title of the link Learn more about John J Audubon.
+ *  $original_image_path - path to download full resolution image.
+ *  $show_download_link - (boll) indicates if user already subscribed.
+ *  $subscribe_link - link to popup with subscribe form.
  */
 ?>
 
 <section class="bird-guide-section right-col boa-plate-illustration center">
   <img src="<?php print $image_thumbnail; ?>" /><div class="clearfix"></div>
   <a href="<?php print $lightbox_image_path; ?>" class="see-full-plate lightboxzoom" ><i class="icon-magnifier"></i> <?php print t('See full plate'); ?></a><div class="clearfix"></div>
-  <a href="<?php print $original_image_path; ?>" class="illustration-attribution" target="blank"><?php print t('Download high-resolution file'); ?></a><div class="clearfix"></div>
+  <?php if ($show_download_link): ?>
+  <a href="<?php print $original_image_path; ?>" class="illustration-attribution" target="blank"><?php print t('Download high-resolution file'); ?></a>
+  <?php else: ?>
+  <?php print $subscribe_link; ?>
+  <?php endif; ?>
+  <div class="clearfix"></div>
   <a href="<?php print $link_url; ?>" class="illustration-attribution"><?php print $link_title; ?></a>
 </section>
