8e1517f58841e614ea5682073c6f9ebf76e1e551
Andrii TIupa <andrii.tiupa@wearepropeople.com>
2015-04-29 16:15:57 +0300

    [ANW-660] improvements

---
+++ docroot/sites/all/modules/custom/nas_panes/plugins/content_types/boa_plate/boa-plate.js
@@ -0,0 +1,12 @@
+(function ($) {
+  Drupal.behaviors.boa_subscribe_popup = {
+    attach: function (context, settings) {
+      $('#modalContent .close.reload').bind('click touchend', function(e) {
+        window.location.reload();
+      });
+      if ($.cookie('show_download_link') !== null && settings.nas_panes.boa_plate_original_image_path !== 'undefined') {
+      	$('#subscribe-link').attr('href', settings.nas_panes.boa_plate_original_image_path);
+      }
+    }
+  }
+})(jQuery);
