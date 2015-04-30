2d7fb1ba269560e22a1831d9ad191199f0df2f0c
Renan Dias <renan@wearepropeople.com.br>
2015-04-30 02:34:55 -0300

    [ANW-661] View changes and template

---
+++ docroot/sites/all/themes/custom/nas/templates/views/views-view-grouping--boa-index.tpl.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @file
+ * This template is used to print a single grouping in a view.
+ *
+ * It is not actually used in default Views, as this is registered as a theme
+ * function which has better performance. For single overrides, the template is
+ * perfectly okay.
+ *
+ * Variables available:
+ * - $view: The view object
+ * - $grouping: The grouping instruction.
+ * - $grouping_level: Integer indicating the hierarchical level of the grouping.
+ * - $rows: The rows contained in this grouping.
+ * - $title: The title of this grouping.
+ * - $content: The processed content output that will normally be used.
+ */
+?>
+<div class="columns boa-bird-card-set bg-boa-bejge">
+  <div class="row section-header space-top">
+    <div class="column">
+      <h2 class="thin boa-family-set-title"><?php print $title; ?></h2>
+    </div>
+  </div>
+  <div class="row boa-bird-card-container clearfix">
+    <?php print $content; ?>
+  </div>
+</div>
