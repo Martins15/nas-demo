5a4f9d376d4fa35c127b67cf1506eea59dfbcd41
Andrii TIupa <andrii.tiupa@wearepropeople.com>
2015-05-11 18:27:24 +0300

    [ANW-660] improvements for BOA popup

--- docroot/sites/all/modules/custom/nas_panes/plugins/content_types/boa_plate/boa-plate.js
+++ docroot/sites/all/modules/custom/nas_panes/plugins/content_types/boa_plate/boa-plate.js
@@ -11,6 +11,20 @@
       	$('#subscribe-link').clone().appendTo('.download-link');
       	$('#subscribe-link:first').remove();
       }
+      $('#modalBackdrop').click(function() {
+         Drupal.CTools.Modal.dismiss();
+      });
+      $('#nas-panes-boa-mail-subscription-form #edit-cancel').bind('click touchend', function(e) {
+        e.preventDefault();
+        $(this).parents('form').remove();
+        $('#modal-content').append('<a target="_blank" href="' + settings.nas_panes.boa_plate_original_image_path + '">' + Drupal.t('Download your image here.') + '</a>');
+        $.cookie('show_download_link', 1, { expires : 365 });
+        $('#subscribe-link').attr('href', settings.nas_panes.boa_plate_original_image_path);
+        $('#subscribe-link').removeClass('ctools-use-modal');
+        $('#subscribe-link').removeClass('ctools-use-modal-processed');
+        $('#subscribe-link').clone().appendTo('.download-link');
+        $('#subscribe-link:first').remove();
+      });
     }
   };
 })(jQuery);
