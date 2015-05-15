3dac28d0e8089fef5656045cef9ef037fbac94e8
Andrii TIupa <andrii.tiupa@wearepropeople.com>
2015-05-15 12:19:42 +0300

    [ANW-660] exclude boa mail image from lazyloader

--- docroot/sites/all/themes/custom/nas/template.php
+++ docroot/sites/all/themes/custom/nas/template.php
@@ -1018,6 +1018,7 @@ function nas_image($variables) {
   // List of styles for not applying of lazyloader.
   $exclude_lazyloader_styles = array(
     'engagement_card',
+    'boa_mail_subscription',
   );
   if (module_exists('lazyloader') && variable_get('lazyloader_enabled', LAZYLOADER_ENABLED) && isset($variables['style_name']) && !in_array($variables['style_name'], $exclude_lazyloader_styles)) {
     $attributes = $variables['attributes'];
