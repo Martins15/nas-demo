9a571bf3d5dc9468cd397046a5da1cc778d8bf0e
Renan Dias <renan@wearepropeople.com.br>
2015-05-21 15:14:57 -0300

    [NAS-52] JS to remove local chapter from chapters and centers

--- docroot/sites/all/modules/custom/nas_panes/plugins/content_types/audubon_near_you_locations/audubon_near_you_locations.js
+++ docroot/sites/all/modules/custom/nas_panes/plugins/content_types/audubon_near_you_locations/audubon_near_you_locations.js
@@ -3,8 +3,14 @@
   Drupal.behaviors.local_chapters_centers_block = {
     attach: function(context, settings) {
       if ($('.top-offset').length) {
+        // Set 'Your Local Chapters' position when displayed.
         var height = $('#audubon-near-you--local-chapter--wrapper').height();
         $('.top-offset > div').css('margin-top', height+30);
+        // Remove Local chapter from Offices & Chapters
+        $('#audubon-near-you--local-chapter--wrapper article').each(function() {
+          var selector = $(this).attr('class');
+          $('#audubon-near-you--office-chapter--wrapper .'+selector).addClass('hide');
+		});
 	  }
     }
   };
