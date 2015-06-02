e4d1605fbb2d10722c361bcb703eca23972e4371
Andrii TIupa <andrii.tiupa@ffw.co>
2015-06-02 12:13:08 +0300

    [ANW-661] Make state birds multiple

---
+++ docroot/sites/all/themes/custom/nas/templates/views/views-view-unformatted--boa-index--boa-index-state.tpl.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php foreach ($rows as $id => $row): ?>
+	<div class="boa-item columns tiny-6 medium-4 large-3 bg-boa-bejge">
+  	<?php print $row; ?>
+  </div>
+<?php endforeach; ?>
