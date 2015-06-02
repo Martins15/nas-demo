8cd673641b06d8ff42a6499c48d32b8043f0ae4d
Andrii TIupa <andrii.tiupa@ffw.co>
2015-06-02 12:16:12 +0300

    tab fix

--- docroot/sites/all/themes/custom/nas/templates/views/views-view-unformatted--boa-index--boa-index-state.tpl.php
+++ docroot/sites/all/themes/custom/nas/templates/views/views-view-unformatted--boa-index--boa-index-state.tpl.php
@@ -8,7 +8,7 @@
  */
 ?>
 <?php foreach ($rows as $id => $row): ?>
-	<div class="boa-item columns tiny-6 medium-4 large-3 bg-boa-bejge">
-  	<?php print $row; ?>
+  <div class="boa-item columns tiny-6 medium-4 large-3 bg-boa-bejge">
+    <?php print $row; ?>
   </div>
 <?php endforeach; ?>
