a94909c4fcffd3f91904febe5cca6ed4a43fd355
podarok <podarok@ua.fm>
2015-08-04 11:18:37 +0300

    Add domain settings file

---
+++ docroot/sites/default/domain.settings.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @file
+ * Per domain settings.
+ */
+
+if (strpos($_SERVER['HTTP_HOST'], 'nas.192.168.56.132.xip.io') !== FALSE) {
+  // Base domain cookie.
+  $cookie_domain = '.nas.192.168.56.132.xip.io';
+
+  // Local environment.
+  $conf['language_domains'] = array(
+    'es' => 'es.nas.192.168.56.132.xip.io',
+    'en' => 'nas.192.168.56.132.xip.io',
+  );
+}
+elseif (strpos($_SERVER['HTTP_HOST'], 'nas.wearepropeople.md') !== FALSE) {
+
+  // Base domain cookie.
+  $cookie_domain = '.nas.wearepropeople.md';
+
+  // Build environment.
+  $conf['language_domains'] = array(
+    'es' => 'es.nas.wearepropeople.md',
+    'en' => 'nas.wearepropeople.md',
+  );
+}
