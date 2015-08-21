43f47962e9d94cf925dc058d30426d94371d3347
Renan Dias <renan@wearepropeople.com.br>
2015-08-21 15:12:23 -0300

    NAS-71: Fix chapter & offices link

--- docroot/sites/all/modules/custom/nas_fpp/templates/flyways-near-you-office-chapter.tpl.php
+++ docroot/sites/all/modules/custom/nas_fpp/templates/flyways-near-you-office-chapter.tpl.php
@@ -6,15 +6,23 @@
 ?>
 
 <h4 class="editorial-card-title blue"><a href="audubon-near-you"><?php print t('Audubon Near You'); ?></a></h4>
+<?php dpm($items); ?>
 <?php foreach ($items as $item) : ?>
   <div class="editorial-card-list-item">
     <h5 class="editorial-card-title no-margin">
-      <a href="node/<?php print $item['nid']; ?>">
+      <?php if ($item['site_address'] != null ): ?>
+        <a href="<?php print $item['site_address']; ?>">
+        <?php print $item['title']; ?>
+          <small class="serif">
+            <em><?php print $item['subtitle']; ?></em>
+          </small>
+         </a>
+      <?php else: ?>
         <?php print $item['title']; ?>
         <small class="serif">
           <em><?php print $item['subtitle']; ?></em>
         </small>
-      </a>
+       <?php endif; ?>
     </h5>
   </div>
 <?php endforeach; ?>
