4ce32bd1f6febc8394227bbd7f291b3cd546d64b
vagrant <podarokua@gmail.com>
2015-09-03 12:38:18 +0000

    Translation #72-73

--- docroot/sites/all/modules/custom/nas_panes/plugins/content_types/boa_next/boa-next.tpl.php
+++ docroot/sites/all/modules/custom/nas_panes/plugins/content_types/boa_next/boa-next.tpl.php
@@ -17,10 +17,10 @@
   <div class="columns">
     <div class="inner">
       <a href="<?php print $prev_url; ?>" class="previous"></a>
-      <span class="previous-text"><a href="<?php print $prev_url; ?>">Plate <?php print $prev_plate_number; ?></a></span>
+      <span class="previous-text"><a href="<?php print $prev_url; ?>"><?php print $prev_plate_number; ?></a></span>
       <span class="previous-title"><a href="<?php print $prev_url; ?>"><?php print $prev_title; ?></a></span>
       <span class="separator"></span>
-      <span class="next-text"><a href="<?php print $next_url; ?>">Plate <?php print $next_plate_number; ?></a></span>
+      <span class="next-text"><a href="<?php print $next_url; ?>"><?php print $next_plate_number; ?></a></span>
       <span class="next-title"><a href="<?php print $next_url; ?>"><?php print $next_title; ?></a></span>
       <a href="<?php print $next_url; ?>" class="next"></a>
     </div>
