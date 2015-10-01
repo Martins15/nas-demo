5e8a209135dd9cbc0587492b9f6d5d492c71fc82
Andrii TIupa <andrii.tiupa@ffw.co>
2015-10-01 16:28:10 +0300

    [ANW-720] Add IBA State info pane, + minor fixes

---
+++ docroot/sites/all/modules/custom/nas_panes/plugins/content_types/iba_state_info/iba-state-info.tpl.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @file
+ * Template for custom pane IBA State Info.
+ *
+ * Available variables:
+ *  $contact_name - (string) Contact Name.
+ *  $contact_title - (string) Contact Title.
+ *  $link - (string) Static link.
+ *  $priorities_data - (array) Contains priorities fields.
+ */
+?>
+
+<p>
+  <strong><?php print $contact_name; ?></strong><br>
+  <?php print $contact_title; ?><br>
+  <?php print $link; ?><br>
+</p>
+<br>
+<h4><?php print t('California IBAs by Type'); ?></h4>
+<table class="data">
+  <thead>
+    <tr>
+      <th><?php print t('IBA Priority'); ?></th>
+      <th><?php print t('Number'); ?></th>
+      <th><?php print t('Acres'); ?></th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach ($priorities_data['priorities'] as $priority): ?>
+    <tr>
+      <td><?php print $priority['pr']; ?></td>
+      <td><?php print $priority['number']; ?></td>
+      <td><?php print $priority['acres']; ?></td>
+    </tr>
+    <?php endforeach; ?>
+  </tbody>
+  <tfoot>
+    <tr>
+      <td><?php print t('Total'); ?></td>
+      <td><?php print $priorities_data['total_number']; ?></td>
+      <td><?php print $priorities_data['total_acres']; ?></td>
+    </tr>
+  </tfoot>
+</table>
