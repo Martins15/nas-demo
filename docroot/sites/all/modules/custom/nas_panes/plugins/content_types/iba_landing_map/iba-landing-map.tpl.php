290cbbf61e6012248e9b92ca654123634b342a79
Andrii TIupa <andrii.tiupa@ffw.co>
2015-11-27 12:45:15 +0200

    [ANW-692] small fixes

--- docroot/sites/all/modules/custom/nas_panes/plugins/content_types/iba_landing_map/iba-landing-map.tpl.php
+++ docroot/sites/all/modules/custom/nas_panes/plugins/content_types/iba_landing_map/iba-landing-map.tpl.php
@@ -7,18 +7,15 @@
  * - $options: (array) contains the list of all IBA states.
  */
 ?>
-<style>
-
-</style>
 <div class="iba-landing-map inline-map">
   <div class="row section-header">
     <div class="columns">
-      <h2 class="thin">Explore IBAs near you</h2>
+      <h2 class="thin"><?php print t('Explore IBAs near you'); ?></h2>
     </div>
     <div class="columns">
       <ul class="iba-state-switcher section-nav inline-list">
         <li>
-          <small>Browse by State</small>
+          <small><?php print t('Browse by State'); ?></small>
           <select>
             <?php foreach ($options as $option): ?>
               <option <?php $option['default'] ? print 'secleted="selected"' : ''; ?> value="<?php print $option['path']; ?>"><?php print $option['title']; ?></option>
@@ -29,6 +26,6 @@
     </div>
   </div>
   <div class="embed-container">
-    <iframe width="500" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="IBA_Website_Audubon" src="<?php print $src; ?>"></iframe>
+    <iframe width="500" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="<?php print t('IBA Website Audubon'); ?>" src="<?php print $src; ?>"></iframe>
   </div>
 </div>
