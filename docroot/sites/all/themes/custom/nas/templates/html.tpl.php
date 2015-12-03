b3f85ce89a4b2bd5515dd33a1f6c534a06cfe96e
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2015-12-03 11:44:13 +0300

    [ANW-784] Fixed notices in browser console.

--- docroot/sites/all/themes/custom/nas/templates/html.tpl.php
+++ docroot/sites/all/themes/custom/nas/templates/html.tpl.php
@@ -46,7 +46,7 @@ global $base_url;
 ?><!doctype html>
 <html lang="en">
   <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0;" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0" />
   <title><?php print $head_title; ?></title>
   <link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600|Noto+Serif:400,700,400italic" rel="stylesheet" type="text/css">
   <?php print $head; ?>
