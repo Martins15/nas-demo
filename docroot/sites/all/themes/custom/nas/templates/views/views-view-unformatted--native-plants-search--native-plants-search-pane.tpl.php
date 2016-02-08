8c088aed3669206d557c8e0b6dba60dee22bb7eb
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2016-02-08 14:05:41 +0300

    [ANW-830] Started theming search results.

--- docroot/sites/all/themes/custom/nas/templates/views/views-view-unformatted--native-plants-search--native-plants-search-pane.tpl.php
+++ docroot/sites/all/themes/custom/nas/templates/views/views-view-unformatted--native-plants-search--native-plants-search-pane.tpl.php
@@ -4,27 +4,222 @@
  * Custom view template to display Native Plants search results.
  */
 ?>
-<h3>Tier1 results</h3>
-<?php foreach ($view->result_tier1 as $result): ?>
-  <div>
-    <span><?php print $result->CommonName; ?></span>
-    <span><?php print $result->ScientificName; ?></span>
-    <span><?php print $result->Family; ?></span>
-    <?php if ($result->Tier1): ?>
-      <span><?php print $result->Description; ?></span>
-    <?php endif; ?>
-    <span><a href="<?php print $result->LocalLink; ?>" target="_blank"><?php print t('Add/edit local info'); ?></a></span>
+<div class="row">
+  <div class="columns">
+    <h2 class="thin">Try these first</h2>
+    <p>These plants should be relatively easy to find and/or grow in your area. <a href="#">Read more.</a></p>
   </div>
-<?php endforeach; ?>
-<h3>All results</h3>
-<?php foreach ($view->result as $result): ?>
-  <div>
-    <span><?php print $result->CommonName; ?></span>
-    <span><?php print $result->ScientificName; ?></span>
-    <span><?php print $result->Family; ?></span>
-    <?php if ($result->Tier1): ?>
-      <span><strong>Tier1</strong></span>
-    <?php endif; ?>
-    <span><a href="<?php print $result->LocalLink; ?>" target="_blank"><?php print t('Add/edit local info'); ?></a></span>
+</div>
+
+<div class="row">
+  <div class="view native-plants-search-results">
+    <div class="view-content">
+      <?php foreach ($view->result_tier1 as $result): ?>
+        <div class="view-row columns">
+          <h3><?php print $result->CommonName; ?> (<em><?php print $result->ScientificName; ?></em>)</h3>
+          <div class="row">
+            <div class="column medium-3">
+              <div class="tier-1-plant-picture hide-for-tiny hide-for-small">
+                <?php print theme('imagecache_external', array(
+                  'path' => $result->PlantImg,
+                  'style_name'=> 'thumbnail',
+                  'alt' => $result->CommonName,
+                )); ?>
+              </div>
+              <div class="row tier-1-plant-picture-mobile hide-for-medium hide-for-large hide-for-xlarge">
+                <?php print theme('imagecache_external', array(
+                  'path' => $result->PlantImg,
+                  'style_name'=> 'thumbnail',
+                  'alt' => $result->CommonName,
+                )); ?>
+              </div>
+            </div>
+            <div class="column medium-4 medium-push-5">
+              <h4><?php print t('Types of birds attracted'); ?></h4>
+              <div class="bird-card-carousel">
+                <div class="row">
+                  <div class="owl-carousel np-owl-processed owl-theme" style="opacity: 1; display: block;">
+                    <!--<div class="bird-card-scroller">-->
+
+                    <div class="owl-wrapper-outer"><div class="owl-wrapper" style="width: 668px; left: 0px; display: block;"><div class="owl-item" style="width: 167px;"><div id="node-452-3" class="node node-bird node-teaser clearfix">
+                            <figure class="bird-card">
+                              <div class="bird-card-illustration">
+                                <a href="/field-guide/bird/snowy-owl"><img src="https://www.audubon.org/sites/default/files/styles/nas_bird_teaser_illustration/public/1780_Sibl_9780307957900_art_r1.jpg?itok=dHlp_lXa" alt=""></a>    </div>
+                              <figcaption class="bird-card-caption">
+                                <h4 class="common-name"><a href="/field-guide/bird/snowy-owl">Owls</a></h4>
+                              </figcaption>
+                            </figure>
+                          </div></div><div class="owl-item" style="width: 167px;"><div id="node-384-3" class="node node-bird node-teaser clearfix">
+                            <figure class="bird-card">
+                              <div class="bird-card-illustration">
+                                <a href="/field-guide/bird/turkey-vulture"><img src="https://www.audubon.org/sites/default/files/styles/nas_bird_teaser_illustration/public/2322_Sibl_9780307957900_art_r1.jpg?itok=YPXZqjSe" alt=""></a>    </div>
+                              <figcaption class="bird-card-caption">
+                                <h4 class="common-name"><a href="/field-guide/bird/turkey-vulture">Vultures</a></h4>
+                              </figcaption>
+                            </figure>
+                          </div></div></div></div>
+
+
+
+                    <div class="owl-controls clickable" style="display: none;"><div class="owl-buttons"><div class="owl-prev disabled"><i class="indicator-left icon-arrow-left"></i></div><div class="owl-next disabled"><i class="indicator-right icon-arrow-right"></i></div></div></div></div>
+                </div>
+              </div>
+            </div>
+            <div class="column medium-5 medium-pull-4 tier-1-plant">
+              <?php if (user_access('administer nodes')): ?>
+                <span><a href="<?php print $result->LocalLink; ?>" target="_blank"><?php print t('Add/edit local info'); ?></a></span>
+              <?php endif; ?>
+              <div class="tier-1-plant--description"><?php print $result->Description; ?></div>
+              <div class="tier-1-plant--source"><?php print('Source'); ?>: <a href="#">Wikipedia</a></div>
+              <ul class="clearfix plant-attributes-list">
+                <?php foreach ($result->Attributes as $attribute): ?>
+                  <li><span style="background-color: <?php print $attribute['color']; ?>;"><?php print $attribute['name']; ?></span></li>
+                <?php endforeach; ?>
+              </ul>
+              <div class="tier-1-plant--add-to-list">
+                <input type="checkbox" id="checkbox-<?php print $result->PlantID; ?>">
+                <label for="checkbox-<?php print $result->PlantID; ?>"><?php print t('Add to your plant list'); ?></label>
+              </div>
+            </div>
+          </div>
+        </div>
+      <?php endforeach; ?>
+    </div>
   </div>
-<?php endforeach; ?>
+</div>
+
+<div class="row">
+  <div class="mobile-results-total-wrapper hide-for-medium hide-for-large hide-for-xlarge">
+    <div class="mobile-results-total columns">
+      <strong><?php print t('Full results'); ?>:</strong> <?php print format_plural($view->query->pager->total_items, '1 plant', '@count plants'); ?>
+    </div>
+  </div>
+  <div class="columns">
+    <h2 class="thin hide-for-tiny hide-form-small"><?php print t('Full results'); ?></h2>
+    <p class="hide-for-tiny hide-for-small"><?php print t('Filter'); ?></p>
+
+    <form action="#" class="inner-filters-wrapper">
+      <select name="" id="s1" placeholder="All types of plants">
+        <option value="">option 1</option>
+        <option value="">option 2</option>
+        <option value="">option 3</option>
+      </select>
+
+      <select name="" id="s2" placeholder="Attracts: Any type of bird">
+        <option value="">option 1</option>
+        <option value="">option 2</option>
+        <option value="">option 3</option>
+      </select>
+    </form>
+  </div>
+
+  <div class="native-plants-full-search-results">
+    <div class="columns">
+      <table class="hide-for-tiny hide-for-small">
+        <thead>
+        <tr>
+          <th><?php print t('Plant name'); ?></th>
+          <th><?php print t('Scientific name'); ?></th>
+          <th><?php print t('Attributes'); ?></th>
+          <th><?php print t('May attract'); ?></th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($view->result as $result): ?>
+          <tr>
+            <td>
+              <div class="plant--add-to-list">
+                <input type="checkbox" id="checkbox-f<?php print $result->PlantID; ?>">
+                <label for="checkbox-f<?php print $result->PlantID; ?>"><?php print $result->CommonName; ?></label>
+                <a class="icon-camera" href="#" title="<?php print t('Preview'); ?>"></a>
+                <?php if (user_access('administer nodes')): ?>
+                  <br><span><a href="<?php print $result->LocalLink; ?>" target="_blank"><?php print t('Add/edit local info'); ?></a></span>
+                <?php endif; ?>
+              </div>
+            </td>
+            <td><em><?php print $result->ScientificName; ?></em></td>
+            <td>
+              <ul class="clearfix plant-attributes-list plant-attributes-list-small">
+                <?php foreach ($result->Attributes as $attribute): ?>
+                  <li><span style="background-color: <?php print $attribute['color']; ?>;"><?php print $attribute['name']; ?></span></li>
+                <?php endforeach; ?>
+              </ul>
+            </td>
+            <td>
+              <a href="#">Orioles</a>,
+              <a href="#">Thrushes</a>,
+              <a href="#">Crows &amp; Jays</a>
+            </td>
+          </tr>
+        <?php endforeach; ?>
+        </tbody>
+      </table>
+    </div>
+
+    <div class="mobile-search-full-results hide-for-medium hide-for-large hide-for-xlarge">
+    <?php foreach ($view->result as $result): ?>
+      <div class="clearfix view-row">
+        <div class="columns">
+          <div>
+            <span class="mobile-search-full-results--label"><?php print t('Plant'); ?>:</span> <?php print $result->CommonName; ?> <a href="#" class="icon-camera"></a>
+          </div>
+          <div>
+            <span class="mobile-search-full-results--label"><?php print t('Scientific name'); ?>:</span> <em><?php print $result->ScientificName; ?></em>
+          </div>
+          <div>
+            <span class="mobile-search-full-results--label"><?php print t('Attributes'); ?>:</span>
+            <?php
+              $attributes = array();
+              foreach ($result->Attributes as $attribute):
+                $attributes[] = '<span style="color: ' . $attribute['color'] . '">' . $attribute['name'] . '</span>';
+              endforeach;
+              print implode(', ', $attributes);
+              ?>
+          </div>
+          <div>
+            <span class="mobile-search-full-results--label"><?php print t('Attracts'); ?>:</span> <a href="#">Insectivores</a>, <a href="#">Seed eaters</a>
+          </div>
+          <div class="plant--add-to-list">
+            <input type="checkbox" id="checkbox-f<?php print $result->PlantID; ?>-s">
+            <label for="checkbox-f<?php print $result->PlantID; ?>-s"><?php print t('Add to your plant list'); ?></label>
+          </div>
+        </div>
+      </div>
+    <?php endforeach; ?>
+    </div>
+
+    <div class="columns">
+      <div class="search-results-total hide-for-tiny hide-for-small">
+        <div class="column medium-4">
+          <strong><?php print t('Results'); ?>:</strong> <?php print format_plural($view->query->pager->total_items, '1 plant', '@count plants'); ?>
+        </div>
+        <div class="column medium-8">
+          <div class="pager">
+            <span class="pager-prev">Previous page</span>
+                <span class="pager-inner">
+                  <span>1</span>
+                  <a href="#">2</a>
+                  <a href="#">3</a>
+                  <a href="#">4</a>
+                  <a href="#">5</a>
+                  <a href="#">6</a>
+                  <a href="#">7</a>
+                  <a href="#">8</a>
+                  <a href="#">9</a>
+                  <span>...</span>
+                  <a href="#">14</a>
+                </span>
+            <a href="#" class="pager-next">Next page</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="search-results-total hide-for-medium hide-for-large hide-for-xlarge row">
+      <div class="pager">
+        <span class="pager-prev">Previous page</span>
+        <a href="#" class="pager-next">Next page</a>
+      </div>
+    </div>
+  </div>
+</div>
