36057fefd8b4d0a42ad464913e255e1cc73c2042
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2016-02-11 18:17:08 +0300

    [ANW-837] Added native_plants_search layout.

---
+++ docroot/sites/all/themes/custom/nas/layouts/native_plants_search/native-plants-search.tpl.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @file
+ * Template for Native Plants search.
+ */
+?>
+<?php print $content['top']; ?>
+<section class="global-content with-padding static-page-content">
+  <?php print $content['main']; ?>
+</section>
+<div class="native-plants-bottom">
+  <?php print $content['bottom']; ?>
+</div>
