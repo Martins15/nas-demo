05d67cd350a4d77ab9d785c00afd834774e28664
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2016-02-12 00:56:20 +0300

    [ANW-838] Added Native Plants hero image pane.

---
+++ docroot/sites/all/modules/custom/nas_panes/plugins/content_types/native_plants_hero/native-plants-hero.tpl.php
@@ -0,0 +1,24 @@
+<?php
+?>
+<div class="bg-respond hero small dark-gradient light-text dark-gradient-bottom">
+  <div class="hero-image">
+    <img src="<?php print $image_path; ?>" alt="" >
+  </div>
+  <div class="row">
+    <div class="hero-header hide-for-medium hide-for-small hide-for-tiny">
+      <div class="columns large-9">
+        <h1 class="hero-title"><?php print $title; ?></h1>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="hero-attribution row">
+  <p class="column"><?php print $image_attribution; ?></p>
+</div>
+<header class="row hide-for-large hide-for-xlarge">
+  <div class="large-12 large-centered columns">
+    <div class="clearfix">
+      <h1><?php print $title; ?></h1>
+    </div>
+  </div>
+</header>
