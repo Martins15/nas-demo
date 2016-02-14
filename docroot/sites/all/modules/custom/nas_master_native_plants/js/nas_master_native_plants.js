783ccbcd303f4dd31356706ea19671f21be0db8e
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2016-02-14 18:39:19 +0300

    [ANW-848] Added taxonomy filters; themed exposed form.

--- docroot/sites/all/modules/custom/nas_master_native_plants/js/nas_master_native_plants.js
+++ docroot/sites/all/modules/custom/nas_master_native_plants/js/nas_master_native_plants.js
@@ -2,6 +2,7 @@
   Drupal.behaviors.nas_master_native_plants = {};
   Drupal.behaviors.nas_master_native_plants.attach = function(context, settings) {
     // Assign classes to the first and last pager items in pagers.
+    // TODO: change the CSS to use first/last child.
     $('.search-results-total .pager', context).each(function() {
       var $pager_items = $(this).children();
       $pager_items.first().addClass('pager-prev');
@@ -9,8 +10,64 @@
     });
   };
 
+  Drupal.behaviors.nas_master_native_plants_filters = {};
+  Drupal.behaviors.nas_master_native_plants_filters.attach = function() {
+    // Do not do anything if there is already cloned select.
+    if ($('#edit-attribute-clone').length) {
+      return;
+    }
+
+    // We can't use context here as pager links do not update the exposed form in block.
+    var $attributes = $('#edit-attribute'),
+      $bird_types = $('#edit-bird-type');
+    if ($attributes.length === 0) {
+      return;
+    }
+
+    // Clone the taxonomy filters and position the clones.
+    var $attributes_clone = $attributes.clone()
+      .attr({'id': $attributes.attr('id') + '-clone', 'name': $attributes.attr('name') + '-clone'})
+      .val($attributes.val())
+      .appendTo('.inner-filters-wrapper').show();
+    var $bird_types_clone = $bird_types.clone()
+      .attr({'id': $bird_types.attr('id') + '-clone', 'name': $bird_types.attr('name') + '-clone'})
+      .val($bird_types.val())
+      .appendTo('.inner-filters-wrapper').show();
+
+    // Update the source select values upon changes on the clones and submit the form.
+    $attributes_clone.change(function() {
+      $attributes.val($attributes_clone.val());
+      $('#edit-submit-native-plants-search').click();
+    });
+    $bird_types_clone.change(function() {
+      $bird_types.val($bird_types_clone.val());
+      $('#edit-submit-native-plants-search').click();
+    });
+  };
+
+  // Click on the attribute filters the view.
+  Drupal.behaviors.nas_master_native_plants_attributes = {};
+  Drupal.behaviors.nas_master_native_plants_attributes.attach = function(context, settings) {
+    $('.native-plants-attribute', context).click(function(event) {
+      event.preventDefault();
+      $('#edit-attribute').val($(this).data('tid'));
+      $('#edit-submit-native-plants-search').click();
+    })
+  };
+
+  // Hide Tier1 results description if there are no Tier1 results.
+  Drupal.behaviors.nas_master_native_plants_tier1 = {};
+  Drupal.behaviors.nas_master_native_plants_tier1.attach = function() {
+    if ($('.native-plants-search-results .view-row').length === 0) {
+      $('.try-these-first').hide();
+    }
+    else {
+      $('.try-these-first').show();
+    }
+  };
+
   /**
-   * Modified Views to ajaxify our pager links.
+   * Modified Views function to ajaxify our pager links.
    */
   Drupal.views.ajaxView.prototype.attachPagerAjax = function() {
     this.$view.find('ul.pager > li > a, th.views-field a, .attachment .views-summary a, .search-results-total .pager a')
--- docroot/sites/all/themes/custom/nas/layouts/native_plants_search/native-plants-search.tpl.php
+++ docroot/sites/all/themes/custom/nas/layouts/native_plants_search/native-plants-search.tpl.php
@@ -6,6 +6,21 @@
 ?>
 <?php print $content['top']; ?>
 <section class="global-content with-padding static-page-content">
+  <div class="row">
+    <div class="text-container columns">
+      <?php print $content['main_top']; ?>
+    </div>
+  </div>
+  <div class="row">
+    <div class="columns">
+      <?php print $content['search_form']; ?>
+    </div>
+  </div>
+  <div class="row try-these-first"">
+    <div class="columns">
+      <?php print $content['main_middle']; ?>
+    </div>
+  </div>
   <?php print $content['main']; ?>
 </section>
 <div class="native-plants-bottom">
