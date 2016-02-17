5a153462858d508689bb4454b1ec66073aaae5d9
Andrey Maximov <savagge@gmail.com>
2016-02-17 20:20:57 +0300

    [ANW-823] Update panes

--- docroot/sites/all/modules/custom/nas_panes/plugins/content_types/native_plants/native_plants_hero/native-plants-hero.tpl.php
+++ docroot/sites/all/modules/custom/nas_panes/plugins/content_types/native_plants/native_plants_hero/native-plants-hero.tpl.php
@@ -1,15 +1,22 @@
 <?php
 ?>
-<div class="bg-respond hero small dark-gradient light-text dark-gradient-bottom">
+<div class="bg-respond hero expand small <?php print $classes; ?>">
   <div class="hero-image">
     <img src="<?php print $image_path; ?>" alt="" >
   </div>
   <div class="row">
     <div class="hero-header hide-for-medium hide-for-small hide-for-tiny">
       <div class="columns large-9">
-        <h1 class="hero-title"><?php print $title; ?></h1>
+        <h1 class="hero-title"><?php print $title_desktop; ?></h1>
       </div>
     </div>
+    <?php if (!$title_mobile_shift): ?>
+      <div class="hero-header hide-for-large hide-for-xlarge">
+        <div class="columns">
+          <h1 class="hero-title"><?php print $title_mobile; ?></h1>
+        </div>
+      </div>
+    <?php endif; ?>
   </div>
 </div>
 <div class="hero-attribution">
@@ -17,10 +24,12 @@
     <span class="hero-attribution-text"><?php print $image_attribution; ?></span>
   </p>
 </div>
-<header class="row hide-for-large hide-for-xlarge">
-  <div class="large-12 large-centered columns">
-    <div class="clearfix">
-      <h1 style="margin:0;"><?php print $title; ?></h1>
+<?php if ($title_mobile_shift): ?>
+  <header class="row hide-for-large hide-for-xlarge space-top">
+    <div class="large-12 large-centered columns">
+      <div class="clearfix">
+        <h1 style="margin:0;"><?php print $title_mobile; ?></h1>
+      </div>
     </div>
-  </div>
-</header>
+  </header>
+<?php endif; ?>
