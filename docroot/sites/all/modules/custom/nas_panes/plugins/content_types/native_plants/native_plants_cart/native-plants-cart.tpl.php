bd385f0adae99b8d57764f6b7f1d2642be2ff1ea
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2016-03-01 19:08:48 +0300

    [ANW-906] Added Clear list functionality.

--- docroot/sites/all/modules/custom/nas_panes/plugins/content_types/native_plants/native_plants_cart/native-plants-cart.tpl.php
+++ docroot/sites/all/modules/custom/nas_panes/plugins/content_types/native_plants/native_plants_cart/native-plants-cart.tpl.php
@@ -6,7 +6,10 @@
 ?>
 <div class="column medium-3 large-3 tiny-6 native-plants-bottom-plant-list-info">
   <h2 class="thin native-plants-bottom--label"><?php print $title; ?></h2>
-  <span class="native-plants-bottom--selected-count"><?php print t('No plants selected'); ?></span>
+  <span class="native-plants-bottom--selected-count">
+    <span class="plants-counter"><?php print t('No plants selected'); ?></span>
+    <span class="clear-plants-list"> | <a href="#" class="native-plants-bottom--clear-plants-list">Clear list</a></span>
+  </span>
 </div>
 <div class="column medium-6 large-5 hide-for-tiny hide-for-small native-plants-bottom-plant-list-items">
   <?php print t('No plants yet'); ?>
