304c37412d060cd78216b36c071fd820668500c3
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2016-03-09 14:16:40 +0300

    [ANW-892] Wrapped Native Plants email form into additional div to correctly highlight contextual link region.

--- docroot/sites/all/modules/custom/nas_panes/plugins/content_types/native_plants/native_plants_email_form/native-plants-email-form.tpl.php
+++ docroot/sites/all/modules/custom/nas_panes/plugins/content_types/native_plants/native_plants_email_form/native-plants-email-form.tpl.php
@@ -4,22 +4,24 @@
  * Native Plants Email form template.
  */
 ?>
-<div class="native-plants-bottom-form-title<?php print !empty($context_links) ? ' contextual-links-region' : ''; ?>">
+<div class="<?php print !empty($context_links) ? 'contextual-links-region' : ''; ?>">
   <?php print $context_links; ?>
-  <div class="row">
-    <div class="column tiny-12">
-      <h2 class="thin hide-for-tiny hide-for-small" style="color: white;"><?php print $title_desktop; ?></h2>
-      <h2 class="thin hide-for-medium hide-for-large hide-for-xlarge" style="color: white;"><?php print $title_mobile; ?></h2>
+  <div class="native-plants-bottom-form-title">
+    <div class="row">
+      <div class="column tiny-12">
+        <h2 class="thin hide-for-tiny hide-for-small" style="color: white;"><?php print $title_desktop; ?></h2>
+        <h2 class="thin hide-for-medium hide-for-large hide-for-xlarge" style="color: white;"><?php print $title_mobile; ?></h2>
+      </div>
     </div>
   </div>
-</div>
-<div class="native-plants-bottom-form">
-  <div class="row">
-    <div class="column medium-7">
-      <?php print $description; ?>
-    </div>
-    <div class="column medium-5">
-      <?php print $form; ?>
+  <div class="native-plants-bottom-form">
+    <div class="row">
+      <div class="column medium-7">
+        <?php print $description; ?>
+      </div>
+      <div class="column medium-5">
+        <?php print $form; ?>
+      </div>
     </div>
   </div>
 </div>
