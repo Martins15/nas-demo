cb179608cd9b8cb15dc710202d738eebb7d03946
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2016-03-09 13:26:34 +0300

    [ANW-892] Implemented contextual link for the Native Plants email form.

---
+++ docroot/sites/all/modules/custom/nas_panes/plugins/content_types/native_plants/native_plants_email_form/native-plants-email-form.tpl.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @file
+ * Native Plants Email form template.
+ */
+?>
+<div class="native-plants-bottom-form-title contextual-links-region">
+  <?php print $context_links; ?>
+  <div class="row">
+    <div class="column tiny-12">
+      <h2 class="thin hide-for-tiny hide-for-small" style="color: white;"><?php print $title_desktop; ?></h2>
+      <h2 class="thin hide-for-medium hide-for-large hide-for-xlarge" style="color: white;"><?php print $title_mobile; ?></h2>
+    </div>
+  </div>
+</div>
+<div class="native-plants-bottom-form">
+  <div class="row">
+    <div class="column medium-7">
+      <?php print $description; ?>
+    </div>
+    <div class="column medium-5">
+      <?php print $form; ?>
+    </div>
+  </div>
+</div>
