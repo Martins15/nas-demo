8f14f814832101c39899f8ee02bb8abfcee17f87
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2016-04-12 18:33:03 +0300

    [ANW-949] Fixed JS to not use the context when copying ZIP code to email form.

--- docroot/sites/all/modules/custom/nas_panes/plugins/content_types/native_plants/native_plants_email_form/js/native_plants_email_form.js
+++ docroot/sites/all/modules/custom/nas_panes/plugins/content_types/native_plants/native_plants_email_form/js/native_plants_email_form.js
@@ -5,10 +5,10 @@
     // We don't use context here cause it doesn't allow us to attach to the refreshed exposed form.
     $('.native-plants-search-form--zip-code').once('native-plants-email-form', function() {
       if ($(this).val()) {
-        $('.native-plants-bottom-form-right--zipcode', context).val($(this).val());
+        $('.native-plants-bottom-form-right--zipcode').val($(this).val());
       }
     }).change(function() {
-      $('.native-plants-bottom-form-right--zipcode', context).val($(this).val());
+      $('.native-plants-bottom-form-right--zipcode').val($(this).val());
     });
   };
 })(jQuery);
