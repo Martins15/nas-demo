d79faab80de8d7836bc3b4b67905361c6ef55729
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2016-04-15 20:50:43 +0300

    [ANW-957] Removed not previously used setting for Foundation equalizer (this setting wasn't there yet in Foundation equalizer 5.4.7)

--- docroot/sites/all/themes/custom/nas/js/mute/foundation.js
+++ docroot/sites/all/themes/custom/nas/js/mute/foundation.js
@@ -5,12 +5,7 @@
 (function($) {
   Drupal.behaviors.equalizer = {
     attach: function(context, settings) {
-      $('body').once('foundation').foundation({
-        equalizer : {
-          // Specify if Equalizer should make elements equal height once they become stacked.
-          equalize_on_stack: true
-        }
-      });
+      $('body').once('foundation').foundation();
     }
   };
 })(jQuery);
