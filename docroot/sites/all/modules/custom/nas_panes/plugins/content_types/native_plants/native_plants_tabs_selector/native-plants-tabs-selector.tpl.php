32270a7fe4174175352825b3691494a647738c98
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2016-08-22 15:06:37 +0300

    [ANW-XXX] Removed deep linking - not working good, conflicts with Views AJAX

--- docroot/sites/all/modules/custom/nas_panes/plugins/content_types/native_plants/native_plants_tabs_selector/native-plants-tabs-selector.tpl.php
+++ docroot/sites/all/modules/custom/nas_panes/plugins/content_types/native_plants/native_plants_tabs_selector/native-plants-tabs-selector.tpl.php
@@ -7,7 +7,7 @@
 <div class="row<?php print !empty($context_links) ? ' contextual-links-region' : ''; ?> <?php print $classes; ?>">
   <?php print $context_links; ?>
   <div class="columns">
-    <ul class="tabs js-tabs hide-for-tiny hide-for-small hide-for-medium" data-tab data-options="deep_linking: true">
+    <ul class="tabs js-tabs hide-for-tiny hide-for-small hide-for-medium" data-tab>
       <li class="tab-title active">
         <a href="#best_results" class="js-ajax-tab js-tab"><?php print $tab1_title; ?></a>
       </li>
