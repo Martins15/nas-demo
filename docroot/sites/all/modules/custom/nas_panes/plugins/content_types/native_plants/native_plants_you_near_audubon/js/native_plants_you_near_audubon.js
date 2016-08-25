5822d0a4bdfc28fb864aa15be859e3d60270eee8
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2016-08-25 18:22:32 +0300

    [ANW-987] Added tab switching to local results tab when clicking on the "you near audubon" pane.

---
+++ docroot/sites/all/modules/custom/nas_panes/plugins/content_types/native_plants/native_plants_you_near_audubon/js/native_plants_you_near_audubon.js
@@ -0,0 +1,13 @@
+(function($) {
+  /**
+   * Tabs.
+   */
+  Drupal.behaviors.you_near_audubon = {};
+  Drupal.behaviors.you_near_audubon.attach = function(context, settings) {
+    var $triggers = $('.native-plants-you-near-audubon .text--blue', context);
+
+    $triggers.on('click', function() {
+      $('ul.tabs li a[href="#local_results"]').click();
+    });
+  };
+})(jQuery);
