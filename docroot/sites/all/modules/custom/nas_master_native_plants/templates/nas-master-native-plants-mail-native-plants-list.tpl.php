daf94ea4acdf5d4449cf59169b7903045972fbb8
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2016-08-31 16:24:27 +0300

    [ANW-979] Added permalink entity creation and redirect to thank-you page.

--- docroot/sites/all/modules/custom/nas_master_native_plants/templates/nas-master-native-plants-mail-native-plants-list.tpl.php
+++ docroot/sites/all/modules/custom/nas_master_native_plants/templates/nas-master-native-plants-mail-native-plants-list.tpl.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0;" />
     <title>National Audubon Society</title>
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600|Noto+Serif:400,700,400italic" rel="stylesheet" type="text/css">
-    <?php if ($css): ?>
+    <?php if (!empty($css)): ?>
       <style type="text/css">
         <!--
         <?php print $css ?>
