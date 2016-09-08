8b2b6dcfdba110b26714b23c100dc1f331796f71
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2016-09-08 10:35:31 +0300

    [ANW-1012] NP-76 - JS solution to update fields values from overriding fields.

--- docroot/sites/all/modules/custom/nas_master_native_plants/js/nas_master_native_plants.fields_sync.js
+++ docroot/sites/all/modules/custom/nas_master_native_plants/js/nas_master_native_plants.fields_sync.js
@@ -2,6 +2,8 @@
   Drupal.behaviors.nas_master_native_plants_fields_sync = {};
   Drupal.behaviors.nas_master_native_plants_fields_sync.attach = function(context, settings) {
     var $inputs = $('input[data-native-plants-fields-sync]', context);
+
+    // Update cookie and fields values on field update.
     $inputs.on('input', function() {
       var fields = Drupal.native_plants_fields_sync.get_fields(),
         key = $(this).data('native-plants-fields-sync'),
@@ -11,11 +13,25 @@
       $inputs.not(this).filter('[data-native-plants-fields-sync="' + key + '"]').val(value);
     });
 
+    // Get fields values from cookie.
     var fields = Drupal.native_plants_fields_sync.get_fields();
+    // Update fields values from overriding fields, update cookie.
+    $inputs.each(function () {
+      if ($(this).data('native-plants-fields-sync-override')) {
+        var key = $(this).data('native-plants-fields-sync'),
+          value = $(this).val();
+        if (value != '') {
+          fields[key] = $(this).val();
+        }
+      }
+    });
+    Drupal.native_plants_fields_sync.set_fields(fields);
+
     if ($.isEmptyObject(fields)) {
       return;
     }
 
+    // Set fields values from cookie.
     $inputs.each(function () {
       var key = $(this).data('native-plants-fields-sync');
       $(this).val(fields[key]);
