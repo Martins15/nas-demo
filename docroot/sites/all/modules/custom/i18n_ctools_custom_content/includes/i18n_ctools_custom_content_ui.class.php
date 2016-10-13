8789cfb3ba6f71e55cb5e248801f2b8fbb4c0108
Andrey Maximov <savagge@gmail.com>
2016-10-13 13:41:12 +0300

    [NAS-272] Introduce i18n_ctools_custom_content module

---
+++ docroot/sites/all/modules/custom/i18n_ctools_custom_content/includes/i18n_ctools_custom_content_ui.class.php
@@ -0,0 +1,24 @@
+<?php
+
+module_load_include('php', 'ctools_custom_content', 'plugins/export_ui/ctools_custom_content_ui.class');
+
+class i18n_ctools_custom_content_ui extends ctools_custom_content_ui {
+
+  function edit_form(&$form, &$form_state) {
+    parent::edit_form($form, $form_state);
+
+    if (empty($form_state['item']->cid)) {
+      $form['language'] = array(
+        '#prefix' => '<div class="messages status">',
+        '#suffix' => '</div>',
+        '#type' => 'markup',
+        '#markup' => t('You are going to create %language translation of the %name custom content', array(
+          '%language' => $form_state['item']->language,
+          '%name' => $form_state['item']->name,
+        )),
+        '#weight' => -1,
+      );
+    }
+  }
+
+}
