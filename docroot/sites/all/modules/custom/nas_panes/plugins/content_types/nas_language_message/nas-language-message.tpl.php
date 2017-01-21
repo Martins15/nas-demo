bd0f62c2f0157115d0bbe86162a75450fb25b893
Ivan Berezhnov <domainsua@gmail.com>
2017-01-21 22:23:54 +0200

    [NAS-359] Fixed language message close button for IE.

--- docroot/sites/all/modules/custom/nas_panes/plugins/content_types/nas_language_message/nas-language-message.tpl.php
+++ docroot/sites/all/modules/custom/nas_panes/plugins/content_types/nas_language_message/nas-language-message.tpl.php
@@ -7,5 +7,5 @@
 <p class="title"><?php print $title; ?></p>
 <p><?php print $spanish_message; ?></p>
 <p><?php print $english_message; ?></p>
-<a class="close button tomato large" href="" onclick="javascript:{modalContentClose();}">OK</a>
+<a class="close button tomato large" href="#" onclick="javascript:{modalContentClose();}">OK</a>
 <div class"image"><?php print $image; ?></div>
