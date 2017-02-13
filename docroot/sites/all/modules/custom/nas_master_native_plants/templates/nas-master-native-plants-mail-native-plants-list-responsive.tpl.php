5be84295319e0257a75b01ae5523c183ac577903
Tiago Bember <tiago.bember@ffwagency.com>
2017-02-13 10:55:06 -0500

    [ANW-1051] Sniffer.

--- docroot/sites/all/modules/custom/nas_master_native_plants/templates/nas-master-native-plants-mail-native-plants-list-responsive.tpl.php
+++ docroot/sites/all/modules/custom/nas_master_native_plants/templates/nas-master-native-plants-mail-native-plants-list-responsive.tpl.php
@@ -1,7 +1,9 @@
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-          "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
+<?php
+/**
+ * @file
+ * Native Plants responsive list template for mail message.
+ */
+?>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
   <head>
     <link rel="stylesheet" type="text/css" href="<?php print $css_url; ?>">
