ac814a247ba8f78db49d62d6098322037a24f959
Ivan Berezhnov <domainsua@gmail.com>
2017-03-09 16:57:39 +0200

    [NAS-381] Add vendor AMP and vendors for AMP.

--- docroot/sites/all/vendor/composer/autoload_namespaces.php
+++ docroot/sites/all/vendor/composer/autoload_namespaces.php
@@ -6,4 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname(dirname($vendorDir)).'/default/files/composer';
 
 return array(
+    'Sabberworm\\CSS' => array($vendorDir . '/sabberworm/php-css-parser/lib'),
+    'QueryPath' => array($vendorDir . '/querypath/querypath/src'),
+    'Ctct' => array($vendorDir . '/constantcontact/constantcontact/src'),
 );
