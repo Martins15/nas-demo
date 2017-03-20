d24706573ba0f708931a1a8bbb99283824eff4fc
bember <bember.tiago@gmail.com>
2017-03-20 13:23:19 -0400

    [ANW-1051] Fixing indentation.

--- docroot/sites/all/modules/custom/nas_panes/plugins/content_types/native_plants/native_plants_nurseries_near_you/native-plants-nurseries-near-you.tpl.php
+++ docroot/sites/all/modules/custom/nas_panes/plugins/content_types/native_plants/native_plants_nurseries_near_you/native-plants-nurseries-near-you.tpl.php
@@ -32,32 +32,32 @@
         </div>
       <?php endforeach; ?>
 
-    <?php if (!empty($nurseries_hidden)): ?>
-      <?php foreach (array_chunk($nurseries_hidden, 3) as $row): ?>
-        <div class="row hidden hide">
-          <?php foreach ($row as $nursery): ?>
-          <div class="large-4 medium-12 columns">
-            <div class="address">
-              <h4><?php print $nursery['title'] ?></h4>
-              <p><?php print $nursery['address']['rendered']; ?></p>
-              <?php if (!empty($nursery['phone'])): ?>
-                <p><?php print $nursery['phone']; ?></p>
-              <?php endif; ?>
-              <?php if (!empty($nursery['link']['url'])): ?>
-                <a target="_blank"
-                   href="<?php print $nursery['link']['url']; ?>"><?php print $nursery['link']['print']; ?></a>
-              <?php endif; ?>
-            </div>
+      <?php if (!empty($nurseries_hidden)): ?>
+        <?php foreach (array_chunk($nurseries_hidden, 3) as $row): ?>
+          <div class="row hidden hide">
+            <?php foreach ($row as $nursery): ?>
+              <div class="large-4 medium-12 columns">
+                <div class="address">
+                  <h4><?php print $nursery['title'] ?></h4>
+                  <p><?php print $nursery['address']['rendered']; ?></p>
+                  <?php if (!empty($nursery['phone'])): ?>
+                    <p><?php print $nursery['phone']; ?></p>
+                  <?php endif; ?>
+                  <?php if (!empty($nursery['link']['url'])): ?>
+                    <a target="_blank"
+                       href="<?php print $nursery['link']['url']; ?>"><?php print $nursery['link']['print']; ?></a>
+                  <?php endif; ?>
+                </div>
+              </div>
+            <?php endforeach; ?>
           </div>
-          <?php endforeach; ?>
+        <?php endforeach; ?>
+        <div class="row">
+          <div class="large-12 medium-12 columns">
+            <a class="show-more-nurseries" href="#">Show more »</a>
+          </div>
         </div>
-      <?php endforeach; ?>
-      <div class="row">
-        <div class="large-12 medium-12 columns">
-          <a class="show-more-nurseries" href="#">Show more »</a>
-        </div>
-      </div>
-    <?php endif; ?>
+      <?php endif; ?>
     </div>
   <?php endif; ?>
 </div>
