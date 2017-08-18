0d55ec3f4b1280de087df7ad3d6ff50e20870085
Artem Ostavnenko <artem.ostavnenko@ffwagency.com>
2017-08-18 20:56:44 +0300

    dot navigation

--- docroot/sites/all/themes/custom/nas/prototypes/video-page-2/video-page.js
+++ docroot/sites/all/themes/custom/nas/prototypes/video-page-2/video-page.js
@@ -7,41 +7,36 @@
     // Init thumbnail preview.
     $('.thumbnail-container ul').owlCarousel({
       loop: false,
-      responsiveClass:true,
+      responsiveClass: true,
       thumbs: true,
       navigation: true,
       items: 5
     });
 
 
-    $('.video-page-section').each(function () {
-      var el = $(this);
-      var activeDotClass = 'active';
-
-      var waypoint = new Waypoint({
-        element: el,
-        handler: function (direction) {
-
-          // For modern browser need to check in ie10
-          var hash = '#' + el.attr('id');
-          history.pushState(null, null, hash);
-
-          // Change class for dot navigation.
-          $('.js-dot-navigation a').removeClass(activeDotClass);
-          $('.js-dot-navigation a[href="' + hash + '"]').addClass(activeDotClass);
-
-        },
-        offset: 0
-      })
-
-    });
+    // $('.video-page-section').each(function () {
+    //   var el = $(this);
+    //   var activeDotClass = 'active';
+    //
+    //   var waypoint = new Waypoint({
+    //     element: el,
+    //     handler: function (direction) {
+    //
+    //       // For modern browser need to check in ie10
+    //       var hash = '#' + el.attr('id');
+    //       history.pushState(null, null, hash);
+    //
+    //       // Change class for dot navigation.
+    //       $('.js-dot-navigation a').removeClass(activeDotClass);
+    //       $('.js-dot-navigation a[href="' + hash +
+    // '"]').addClass(activeDotClass);  }, offset: 0 })  });
 
     // Sticky dot navigation.
-    var stickyDotNavigation = new Waypoint.Sticky({
-      element: $('.js-dot-navigation')[0],
-      stuckClass: 'is-fixed',
-      wrapper: false
-    });
+    // var stickyDotNavigation = new Waypoint.Sticky({
+    //   element: $('.js-dot-navigation')[0],
+    //   stuckClass: 'is-fixed',
+    //   wrapper: false
+    // });
 
 
     // Thumbnail auto play.
@@ -50,11 +45,7 @@
       if (this.paused) {
         this.play();
       }
-    }).on('mouseleave', function () {
-      if (!this.paused) {
-        this.pause();
-      }
-    });
+    })
 
 
     // Animated anchor link scroll.
@@ -72,13 +63,54 @@
     });
 
 
-    $('video').each(function(){
-      if ($(this).is(":in-viewport")) {
-        $(this)[0].play();
-      } else {
-        $(this)[0].pause();
-      }
-    })
+    // $('video').each(function(){
+    //   if ($(this).is(":in-viewport")) {
+    //     $(this)[0].play();
+    //   } else {
+    //     $(this)[0].pause();
+    //   }
+    // })
+
+
+    // Dot placeholder.
+    function dotNavigation() {
+      var $dotContainer = $('.dot-navigation')
+          , $dotList = $('ul', $dotContainer)
+          , $link = $('a', $dotContainer);
+
+      // Dot title.
+      $link.each(function () {
+        var $el = $(this)
+            , linkText = $el.data('text');
+        console.log(linkText)
+        $el.next().text(linkText);
+      });
+
+      // Dot carousel
+      $dotList.slick({
+        centerMode: false,
+        slidesToShow: 5,
+        infinite: false,
+        arrows: false,
+        responsive: [
+
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              slidesToShow: 3
+            }
+          }
+        ]
+
+      });
+
+
+    }
+
+
+
+    dotNavigation();
 
   })
 
