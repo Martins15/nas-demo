fc656953457a045bfe3b210a3534bac3c08961a8
Artem Ostavnenko <artem.ostavnenko@ffwagency.com>
2017-08-23 17:54:05 +0300

    video-page-2: swipe for thumb carousel. Remove owl carousel, add slick slider

--- docroot/sites/all/themes/custom/nas/prototypes/video-page-2/video-page.js
+++ docroot/sites/all/themes/custom/nas/prototypes/video-page-2/video-page.js
@@ -88,23 +88,64 @@
           .removeClass(thumbClass)
           .addClass(copyThumbClass);
 
-      // Main Top carousel.
-      $thumbContainerOrigin.owlCarousel({
-        loop: false,
-        responsiveClass: true,
-        thumbs: true,
-        navigation: true,
-        items: 5
+      $thumbContainerOrigin.slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 5,
+              slidesToScroll: 1,
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          }
+          ]
       });
 
-      // Bottom carousel.
-      $('.js-thumbnail-secondary ul').owlCarousel({
-        loop: false,
-        responsiveClass: true,
-        thumbs: true,
-        navigation: true,
-        items: 5
+      $('.js-thumbnail-secondary ul').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 5,
+              slidesToScroll: 1,
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          }
+        ]
       });
+
     }
 
   };
