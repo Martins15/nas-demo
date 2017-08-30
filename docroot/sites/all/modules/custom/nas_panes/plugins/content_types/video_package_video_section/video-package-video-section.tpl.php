997481401c534a643d3a7b49b2c64719a26d7626
AndreyMaximov <savagge@gmail.com>
2017-08-30 14:29:36 +0300

    [NAS-523] Fix container

---
+++ docroot/sites/all/modules/custom/nas_panes/plugins/content_types/video_package_video_section/video-package-video-section.tpl.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file
+ * Template for Video Package Video Section.
+ */
+?>
+
+<!-- Video package video section -->
+<div id="<?php print $hash; ?>" class="clearfix video-container">
+  <video muted playsinline class="main-video-item" poster="<?php print $poster; ?>"
+         data-src="<?php print $video; ?>"
+         data-thumb-src="<?php print $thumbnail_video; ?>"
+         data-thumb-poster="<?php print $thumbnail_poster; ?>"
+         data-short-title="<?php print $title_short; ?>">
+    <source src="" type="video/mp4">
+  </video>
+  <div class="video-content column white-text" data-top="100px" data-bottom="">
+    <h1><?php print $title_long; ?></h1>
+  </div>
+</div>
+<!-- /Video package video section -->
