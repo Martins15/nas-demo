6010898d0b449191ddcbb2c516d4bcff57319c79
Alex Pronchuk <alex.pronchuk@ffwagency.com>
2017-11-16 17:10:37 +0200

    NAS-561 removed changes for old form.

--- docroot/sites/all/modules/custom/nas_panes/plugins/content_types/native_plants/native_plants_initial_form/native-plants-initial-form.tpl.php
+++ docroot/sites/all/modules/custom/nas_panes/plugins/content_types/native_plants/native_plants_initial_form/native-plants-initial-form.tpl.php
@@ -5,12 +5,12 @@
  * Template for Native Plants initial form.
  */
 ?>
-<div class="row element-hidden">
+<div class="row">
   <div class="columns">
     <?php print render($form['email']); ?>
   </div>
 </div>
-<div class="row element-hidden">
+<div class="row">
   <div class="columns medium-6">
     <?php print render($form['zipcode']); ?>
   </div>
@@ -18,6 +18,5 @@
     <?php print render($form['submit']); ?>
   </div>
 </div>
-<div class="element-hidden">
-  <?php print drupal_render_children($form); ?>
-</div>
+<?php print drupal_render_children($form); ?>
+
