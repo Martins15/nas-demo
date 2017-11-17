ed5836a2b654c95f4d1327dea2c78f73b4b5d296
Alex <alexey.belyaev@ffwagency.com>
2017-11-17 16:07:56 +0200

    [NAS-548] add hero video fields for articles, templates, handlers

---
+++ docroot/sites/all/themes/custom/nas/layouts/nas_article_fullscreen_video/nas-article-fullscreen-video.tpl.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * @file
+ * Template of Article layout with Fullscreen image.
+ *
+ * Available variables:
+ *  - $color_mode_text: text color mode for hero image.
+ *  - $color_mode_gradient: gradient color mode for hero image.
+ *
+ * @impove
+ *   These variables only available when rendering in NAS theme, so have to be
+ *   sure they are initialized or notices will be generated in panels edit
+ *   interface.
+ *
+ * @see nas_preprocess_nas_article_fullscreen()
+ */
+?>
+<?php print $content['magazine_bar']; ?>
+<div class="hero <?php print !empty($color_mode_gradient) ? $color_mode_gradient : 'dark'; ?>-gradient <?php print !empty($color_mode_text) ? $color_mode_text : ''; ?>-text expand">
+  <div class="hero-video">
+    <?php print $content['big_image']; ?>
+  </div> 
+  <div class="row">
+    <div class="hero-header hide-for-medium hide-for-small hide-for-tiny">
+      <div class="columns large-10 large-centered">
+        <?php print $content['header_hero']; ?>
+      </div>
+    </div>
+    <?php if (trim($content['header_hero_attr_text'])): ?>
+      <div class="hero-attribution-fullscreen">
+        <div class="hero-attribution-text extra">
+          <?php print $content['header_hero_attr_text']; ?>
+        </div>
+      </div>
+    <?php endif; ?>
+  </div>
+</div>
+<section class="global-content">
+  <article class="article">
+    <header class="article-header row">
+      <div class="large-10 large-centered columns hide-for-large hide-for-xlarge">
+        <?php print $content['header']; ?>
+        <div class="article-meta clearfix hide-for-large hide-for-xlarge">
+          <?php print $content['mobile_author']; ?>
+        </div>
+      </div>
+    </header>
+    <div class="article-body row">
+      <div class="article-sidebar large-2 columns">
+        <?php print $content['social']; ?>
+        <section class="clearfix article-sidebar-section article-meta hide-for-tiny hide-for-small hide-for-medium">
+          <?php print $content['author']; ?>
+        </section>
+        <section class="clearfix article-sidebar-section article-related-birds reflow reflow-into-body" data-reflow-class="article-aside half-width">
+          <?php print $content['birds']; ?>
+        </section>
+        <?php print $content['left']; ?>
+      </div>
+      <div class="article-text large-10 columns reflow-body">
+        <aside class="article-aside reflow reflow-into-body">
+          <?php print $content['right']; ?>
+        </aside>
+        <?php print $content['main']; ?>
+        <?php if(!empty($content['file_attachments'])): ?>
+        <div class="file-attachments">
+          <h3><?php print t('Downloadable Resources'); ?></h3>
+          <div class="files-list">
+            <?php print $content['file_attachments']; ?>
+          </div>
+        </div>
+        <?php endif;?>
+      </div>
+    </div>
+  </article>
+  <?php print $content['related']; ?>
+  <div class="row">
+    <div class="column">
+      <?php print $content['comments']; ?>
+    </div>
+  </div>
+  <section class="card-set reset-white">
+    <?php print $content['card_set']; ?>
+  </section>
+  <?php print $content['article_more_features']; ?>
+</section>
---
+++ docroot/sites/all/themes/custom/nas/layouts/nas_article_standard_hero_video/nas-article-standard-hero-video.tpl.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * @file
+ * Template of Article layout with Big image.
+ */
+?>
+<div class="hero dark-gradient">
+  <div class="hero-video">
+    <?php print $content['big_image']; ?>
+  </div>
+</div>
+<?php if (!empty($content['header_hero_attr_text'])): ?>
+<div class="hero-caption">
+  <div class="row">
+    <div class="caption large-10 large-centered columns">
+      <p><?php print $content['header_hero_attr_text']; ?></p>
+    </div>
+  </div>
+</div>
+<?php endif; ?>
+<section class="global-content">
+  <article class="article">
+    <header class="article-header row">
+      <div class="large-10 large-centered columns">
+        <?php print $content['header']; ?>
+        <div class="article-meta clearfix hide-for-large hide-for-xlarge">
+          <?php print $content['mobile_author']; ?>
+        </div>
+      </div> 
+    </header>
+    <div class="article-body row">
+      <div class="article-sidebar large-2 columns">
+        <?php print $content['social']; ?>
+        <section class="clearfix article-sidebar-section article-meta hide-for-tiny hide-for-small hide-for-medium">
+          <?php print $content['author']; ?>
+        </section>
+        <section class="clearfix article-sidebar-section article-related-birds reflow reflow-into-body" data-reflow-class="article-aside half-width">
+          <?php print $content['birds']; ?>
+        </section>
+        <?php print $content['left']; ?>
+      </div>
+      <div class="article-text large-10 columns reflow-body">
+        <aside class="article-aside reflow reflow-into-body">
+          <?php print $content['right']; ?>
+        </aside>
+        <?php print $content['main']; ?>
+        <?php if(!empty($content['file_attachments'])): ?>
+        <div class="file-attachments">
+          <h3><?php print t('Downloadable Resources'); ?></h3>
+          <div class="files-list">
+            <?php print $content['file_attachments']; ?>
+          </div>
+        </div>
+        <?php endif;?>
+      </div>
+    </div>
+  </article>
+  <?php print $content['related']; ?>
+  <div class="row">
+    <div class="column">
+      <?php print $content['comments']; ?>
+    </div>
+  </div>
+  <section class="card-set reset-white">
+    <?php print $content['card_set']; ?>
+  </section>
+</section>
