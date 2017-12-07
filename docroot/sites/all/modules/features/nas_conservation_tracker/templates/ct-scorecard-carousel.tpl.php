cf08ac79b4631b7b6b7ec418afb19cdd0c4783d7
Martin Malovanets <martin.malovanets@ffwagency.com>
2017-12-07 12:38:44 +0200

    Fixed errors in tpls

--- docroot/sites/all/modules/features/nas_conservation_tracker/templates/ct-scorecard-carousel.tpl.php
+++ docroot/sites/all/modules/features/nas_conservation_tracker/templates/ct-scorecard-carousel.tpl.php
@@ -4,6 +4,8 @@
  */
 ?>
 <div class="ct-scorecard-carousel">
+  <?php print $title; ?>
+  <?php //var_dump($items); ?>
   <?php foreach ($items as $item): ?>
     <?php print $item; ?>
   <?php endforeach; ?>
--- docroot/sites/all/modules/features/nas_conservation_tracker/templates/ct-scorecard-tabs.tpl.php
+++ docroot/sites/all/modules/features/nas_conservation_tracker/templates/ct-scorecard-tabs.tpl.php
@@ -6,9 +6,11 @@
 <div class="ct-scorecard-tabs">
   <?php foreach ($menu_tree as $item): ?>
     <?php print $item['markup']; ?>
+    <?php if (isset($item['children'])): ?>
     <?php foreach ($item['children'] as $child): ?>
       <?php print $child; ?>
     <?php endforeach; ?>
+    <?php endif; ?>
   <?php endforeach; ?>
   <?php foreach ($tabs as $tab): ?>
     <?php print $tab; ?>
