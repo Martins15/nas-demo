81098428c69199a46d00a73d8efbf50f8f41c90f
Alex Pronchuk <alex.pronchuk@ffwagency.com>
2017-12-15 16:51:31 +0200

    [NAS-552] reverted.

--- docroot/sites/all/themes/custom/nas_amp/templates/content/node--article.tpl.php
+++ docroot/sites/all/themes/custom/nas_amp/templates/content/node--article.tpl.php
@@ -91,7 +91,6 @@
       <?php print $content['article_slug']; ?>
       <h1 class="hg-title"><?php print $title; ?></h1>
       <?php print render($content['field_subtitle']); ?>
-      <?php print render($content['byline']); ?>
       <?php print render($content['field_article_date']); ?>
     </header>
 
