e0bdfe7b6070db35456fda8bbf8a20ef9ac2fef3
Martin Malovanets <martin.malovanets@ffwagency.com>
2017-12-18 15:03:57 +0200

    Merge code

--- docroot/sites/all/modules/features/nas_conservation_tracker/templates/ct-scorecard-main.tpl.php
+++ docroot/sites/all/modules/features/nas_conservation_tracker/templates/ct-scorecard-main.tpl.php
@@ -38,3 +38,16 @@
     </div>
   </div>
 </aside>
+
+<div class="tabs" data-ng-controller="Tabs">
+  <span>$state = <b>{{$state.current.name}}</b></span><br>
+  <span>$state url = <b>{{$state.$current.url.source}}</b></span>
+  <ul>
+    <li
+      ng-repeat="tab in tabs"
+      heading="{{ tab.heading }}"
+      ui-sref="{{ tab.route }}"
+      active="tab.active">{{ tab.heading }}
+    </li>
+  </ul>
+</div>
