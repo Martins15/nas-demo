cf9dda5fd069cbe45ca437e0afef48b5931e11d2
andrew chernous <andrew.chernous@ffwagency.com>
2017-12-18 20:16:30 +0200

    Update templates.

--- docroot/sites/all/modules/features/nas_conservation_tracker/templates/ct-scorecard-overview.tpl.php
+++ docroot/sites/all/modules/features/nas_conservation_tracker/templates/ct-scorecard-overview.tpl.php
@@ -3,6 +3,29 @@
  * TPL file for CT content type.
  */
 ?>
+<div class="tabs ct-item-nav-conservation" ng-cloak data-ng-controller="Tabs">
+    <div class="row">
+        <div class="menu-wrap columns large-6">
+            <ul>
+                <li
+                        ng-repeat="tab in tabs"
+                        ui-sref="{{ tab.route }}"
+                        class="{{tab.heading}} {{tab.active}}">{{ tab.heading }}
+                </li>
+            </ul>
+        </div>
+        <div class="text-wrap columns large-6">
+            <div class="text-wrap__content" ng-cloak data-ng-controller="Content">
+                <div ng-repeat="item in content">
+                    <h4>{{item.title}}<h4>
+                            <p>{{item.text}}<p>
+                </div>
+            </div>
+        </div>
+</div>
+</div>
+
+
 <div class="ct-scorecard-overview row">
   <div class="bird-page body-item">
     <div class="title-wrap">
