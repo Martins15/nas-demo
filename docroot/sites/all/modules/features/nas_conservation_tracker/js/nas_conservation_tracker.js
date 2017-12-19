5e3728866eecc936c5b15649d09198edc8157ef6
Martin Malovanets <martin.malovanets@ffwagency.com>
2017-12-19 16:12:45 +0200

    highlight states with sites

--- docroot/sites/all/modules/features/nas_conservation_tracker/js/nas_conservation_tracker.js
+++ docroot/sites/all/modules/features/nas_conservation_tracker/js/nas_conservation_tracker.js
@@ -6,11 +6,42 @@
 (function ($, Drupal) {
 
   Drupal.behaviors.nas_conservation_tracker = {
-    attach:function (context, settings) {
-      console.log(Drupal.settings.nas_conservation_tracker_states_data);
-      console.log(Drupal.settings.leaflet['0'].lMap);
-      L.geoJson(Drupal.settings.nas_conservation_tracker_states_data).addTo(Drupal.settings.leaflet['0'].lMap);
-      //Drupal.leaflet.create_polygon(Drupal.settings.nas_conservation_tracker_states_data, Drupal.settings.leaflet['0'].lMap);
+    attach: function (context, settings) {
+      // Highlight states which have sites.
+      var polygons = [],
+        states = {};
+      for (var i = 0 in settings.nas_conservation_tracker.json_data.actions.sites) {
+        var stateID = settings.nas_conservation_tracker.json_data.actions.sites[i].state;
+        if (Number.isInteger(states[stateID])) {
+          states[stateID]++;
+        }
+        else {
+          states[stateID] = 0;
+        }
+      }
+      for (var j = 0 in settings.nas_conservation_tracker_states_data) {
+        if (typeof settings.nas_conservation_tracker_states_data[j].properties.shortname == 'string' &&
+          states[settings.nas_conservation_tracker_states_data[j].properties.shortname] > 0) {
+          polygons.push(settings.nas_conservation_tracker_states_data[j]);
+        }
+      }
+      L.geoJson({type: 'FeatureCollection', features: polygons}, {style: getStateStyle}).addTo(settings.leaflet['0'].lMap);
+
+      // Display sites (dots).
+
+      // Helper functions.
+      function getStateStyle(feature) {
+        var d = states[feature.properties.shortname],
+        color = d > 10 ? '#FEB24C' :
+          d > 5 ? '#FED976' : '#FFEDA0';
+        return {
+          fillColor: color,
+          weight: 2,
+          opacity: 1,
+          color: 'black',
+          fillOpacity: 1
+        }
+      }
     }
   };
 
