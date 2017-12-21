b00260b50d788769fb205f49c5fe4f7b339da144
andrew chernous <andrew.chernous@ffwagency.com>
2017-12-21 18:31:37 +0200

    Hide empty block.

--- docroot/sites/all/modules/features/nas_conservation_tracker/templates/ct-scorecard-carousel.tpl.php
+++ docroot/sites/all/modules/features/nas_conservation_tracker/templates/ct-scorecard-carousel.tpl.php
@@ -3,10 +3,10 @@
  * TPL file for CT content type.
  */
 ?>
-<div class="ct-scorecard-carousel">
-  <?php print $title; ?>
-  <?php //var_dump($items); ?>
-  <?php foreach ($items as $item): ?>
-    <?php print $item; ?>
-  <?php endforeach; ?>
-</div>
+<!--<div class="ct-scorecard-carousel">-->
+<!--  --><?php //print $title; ?>
+<!--  --><?php ////var_dump($items); ?>
+<!--  --><?php //foreach ($items as $item): ?>
+<!--    --><?php //print $item; ?>
+<!--  --><?php //endforeach; ?>
+<!--</div>-->
