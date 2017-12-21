7fbfdbf48b470e64fa08b57d669e3348cd44a4af
andrew chernous <andrew.chernous@ffwagency.com>
2017-12-21 14:54:35 +0200

    Add style for breadcrumbs.

--- docroot/sites/all/modules/features/nas_conservation_tracker/templates/ct-scorecard-tabs.tpl.php
+++ docroot/sites/all/modules/features/nas_conservation_tracker/templates/ct-scorecard-tabs.tpl.php
@@ -4,15 +4,14 @@
  */
 ?>
 <div class="ct-scorecard-tabs">
-  <?php foreach ($menu_tree as $item): ?>
-    <?php print $item['markup']; ?>
-    <?php if (isset($item['children'])): ?>
-    <?php foreach ($item['children'] as $child): ?>
-      <?php print $child; ?>
-    <?php endforeach; ?>
-    <?php endif; ?>
-  <?php endforeach; ?>
-  <?php foreach ($tabs as $tab): ?>
-    <?php print $tab; ?>
-  <?php endforeach; ?>
+    <div class="row">
+      <?php foreach ($menu_tree as $item): ?>
+        <?php print $item['markup']; ?>
+        <?php if (isset($item['children'])): ?>
+          <?php foreach ($item['children'] as $child): ?>
+            <?php print $child; ?>
+          <?php endforeach; ?>
+        <?php endif; ?>
+      <?php endforeach; ?>
+    </div>
 </div>
