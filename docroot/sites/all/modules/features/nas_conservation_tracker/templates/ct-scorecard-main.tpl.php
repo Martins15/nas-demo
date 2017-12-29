0a291b6ed9e9fd5eb948a1d7ef7d4d9026f263aa
Alex Davidovic <alexandar87@gmail.com>
2017-12-29 15:30:40 +0100

    Task-51: Added placeholder for actions.

--- docroot/sites/all/modules/features/nas_conservation_tracker/templates/ct-scorecard-main.tpl.php
+++ docroot/sites/all/modules/features/nas_conservation_tracker/templates/ct-scorecard-main.tpl.php
@@ -4,43 +4,50 @@
  */
 ?>
 <aside class="ct-scorecard-main map-items bird-page">
-    <div>
+  <div>
     <?php print $form; ?>
     <a href="#" class="controls-link"><span class="border"></span> </a>
   </div>
-    <div class="wrap-map-diagram">
+  <div class="wrap-map-diagram">
     <?php print $map; ?>
-      <div class="help-wrap-items-map-items">
-          <div class="diagram-wrap help-bg-white columns map-item large-8">
-              <div class="map-item__inner-help">
-                  <div class="diagram-wrap__title">
-                      <h4 class="map-title">Action Categories <span>?</span></h4>
-                      <p>Sites: <span  id="site-count">0</span></p>
-                  </div>
-                  <div class="diagram-item">
-                    <?php print $actions; ?>
-                  </div>
+    <div class="help-wrap-items-map-items">
+      <div class="diagram-wrap help-bg-white columns map-item large-8">
+        <div class="map-item__inner-help">
+          <div class="diagram-wrap__title">
+            <h4 class="map-title">Action Categories</h4>
+            <div class="trigger">
+              <h5>Display Actions</h5>
+              <div class="switch">
+                <input type="checkbox" class="form-switch" id="checkActions">
+                <label for="checkActions"></label>
               </div>
+            </div>
+            <p>Sites: <span id="site-count">0</span></p>
           </div>
-          <div class="objectives-wrap help-bg-white columns map-item large-8">
-              <div class="map-item__inner-help">
-                  <div class="objectives-wrap__title">
-                      <h4 class="map-title">Objectives <span>?</span></h4></div>
-                  <div class="objectives-wrap__wrap-items">
-                    <?php print $objectives; ?>
-                  </div>
-              </div>
+          <div class="diagram-item">
+            <?php print $actions; ?>
           </div>
-          <div class="progress-wrap map-item large-8">
-              <div class="map-item__inner-help">
-                  <div class="progress-wrap__title">
-                      <h4 class="map-title">Overall Progress</h4></div>
-                  <div class="progress-item">
-                      <p style="width:71%" data-value="71"></p>
-                      <progress max="100" value="71" class="html5"></progress>
-                  </div>
-              </div>
+        </div>
+      </div>
+      <div class="objectives-wrap help-bg-white columns map-item large-8">
+        <div class="map-item__inner-help">
+          <div class="objectives-wrap__title">
+            <h4 class="map-title">Objectives</h4></div>
+          <div class="objectives-wrap__wrap-items">
+            <?php print $objectives; ?>
           </div>
+        </div>
+      </div>
+      <div class="progress-wrap map-item large-8">
+        <div class="map-item__inner-help">
+          <div class="progress-wrap__title">
+            <h4 class="map-title">Overall Progress</h4></div>
+          <div class="progress-item">
+            <p style="width:71%" data-value="71"></p>
+            <progress max="100" value="71" class="html5"></progress>
+          </div>
+        </div>
       </div>
     </div>
+  </div>
 </aside>
