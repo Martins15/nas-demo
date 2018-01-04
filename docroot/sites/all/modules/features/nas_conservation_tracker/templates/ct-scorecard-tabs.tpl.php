b81270f2b908f79bfddd2f2475dea3fc2058f4b4
Artem Ostavnenko <artem.ostavnenko@ffwagency.com>
2018-01-04 11:57:40 +0200

    Breadcrumb styles with dropdown

--- docroot/sites/all/modules/features/nas_conservation_tracker/templates/ct-scorecard-tabs.tpl.php
+++ docroot/sites/all/modules/features/nas_conservation_tracker/templates/ct-scorecard-tabs.tpl.php
@@ -3,25 +3,27 @@
  * TPL file for CT content type.
  */
 ?>
+<div class="ct-scorecard-tabs">
+  <div class="row">
+    <?php foreach ($menu_tree as $item): ?>
+      <span class="current-title"><?php print $item['markup']; ?></span>
+      <?php if (isset($item['children'])): ?>
+        <?php foreach ($item['children'] as $child): ?>
+          <?php print $child['markup']; ?>
+        <?php endforeach; ?>
+      <?php endif; ?>
+    <?php endforeach; ?>
+  </div>
+</div>
+
+
 <!--<div class="ct-scorecard-tabs">-->
 <!--  <div class="row">-->
-<!--    --><?php //foreach ($menu_tree as $item): ?>
-<!--      <span class="current-title">--><?php //print $item['markup']; ?><!--</span>-->
-<!--      --><?php //if (isset($item['children'])): ?>
-<!--        --><?php //foreach ($item['children'] as $child): ?>
-<!--          --><?php //print $child['markup']; ?>
-<!--        --><?php //endforeach; ?>
-<!--      --><?php //endif; ?>
-<!--    --><?php //endforeach; ?>
+<!--    <span class="current-title"><a href="/">Conservation Tracker</a></span>-->
+<!--    <a href="/" class="active-trail">Landscapes</a>-->
+<!--    <a href="/">Species</a>-->
+<!--    <a href="/">Strategies</a>-->
+<!--    <span class="current-title">Black Oystercatcher</span>-->
 <!--  </div>-->
 <!--</div>-->
 
-<div class="ct-scorecard-tabs">
-  <div class="row">
-    <span class="current-title"><a href="/">Conservation Tracker</a></span>
-    <a href="/" class="active-trail">Landscapes</a>
-    <a href="/">Species</a>
-    <a href="/">Strategies</a>
-    <span class="current-title">Black Oystercatcher</span>
-  </div>
-</div>
