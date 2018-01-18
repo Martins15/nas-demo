92905151c53b876a7a36c976779955d6ddae6c58
Martin Malovanets <martin.malovanets@ffwagency.com>
2018-01-18 15:33:27 +0200

    fix

--- docroot/sites/all/themes/custom/nas/templates/views/views-view.tpl.php
+++ docroot/sites/all/themes/custom/nas/templates/views/views-view.tpl.php
@@ -52,7 +52,7 @@
   <?php endif; ?>
 
   <?php if ($rows): ?>
-    <div class="view-content <?php print $equalizer ? ' data-equalizer ' : ''; ?>">
+    <div class="view-content"<?php print $equalizer ? ' data-equalizer ' : ''; ?>>
       <?php print $rows; ?>
     </div>
   <?php elseif ($empty): ?>
