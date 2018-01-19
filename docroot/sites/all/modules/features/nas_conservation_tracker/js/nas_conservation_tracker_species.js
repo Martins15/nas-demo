7e15870addb6d701836c284c344bfee131b5016d
andrew chernous <andrew.chernous@ffwagency.com>
2018-01-19 19:15:01 +0200

    Update js and style for filter on /conservation-tracker/species page.

--- docroot/sites/all/modules/features/nas_conservation_tracker/js/nas_conservation_tracker_species.js
+++ docroot/sites/all/modules/features/nas_conservation_tracker/js/nas_conservation_tracker_species.js
@@ -39,4 +39,33 @@
       })
     }
   };
+
+  Drupal.behaviors.filterUppdate = {
+    attach: function (context, settings) {
+      // Insert block to another block.
+      $('#views-exposed-form-ct-species-default').once(function () {
+        var threatWrap = $('#edit-threat-status .fieldset-wrapper', context);
+        $('.form-item', threatWrap).wrapAll('<div class="wrap-items"><div class="help-items"></div>');
+        $('<div class="fieldset-title second">Audubon Climate<span class="fieldset-legend-prefix"></span></div>').prependTo('.wrap-items');
+        $('#edit-iucn-wrapper',context).insertAfter('.wrap-items');
+      });
+
+      var wrapItems = $('.help-items');
+      var wrapMainItems = $('.wrap-items');
+      var classHide = 'js-close';
+
+      $('.fieldset-title.second').click(function (e) {
+        e.preventDefault();
+        if (wrapItems.hasClass(classHide)) {
+          wrapItems.slideDown('fast').removeClass('js-close');
+          $('span', this).removeClass('js-down');
+        } else {
+          wrapItems.slideUp('fast').addClass('js-close');
+          $('span', this).addClass('js-down');
+        }
+
+      })
+    }
+  };
+
 })(jQuery, window.Drupal);
