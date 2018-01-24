3321133a41ec160419f825c768a04aac42152a8f
Alex Pronchuk <alex.pronchuk@ffwagency.com>
2018-01-24 13:49:03 +0200

    Strategies lp fixes and progress.

--- docroot/sites/all/modules/features/nas_conservation_tracker/templates/ct-strategies-main.tpl.php
+++ docroot/sites/all/modules/features/nas_conservation_tracker/templates/ct-strategies-main.tpl.php
@@ -3,16 +3,18 @@
  * TPL file for CT content type.
  */
 ?>
-<section class="ct-landscapes-main">
+<section class="ct-strategies-main">
   <div class="row">
     <div ng-app="nasCtStrategies" ng-controller="tabs">
       <tabset>
         <tab ng-repeat="tab in tabs" heading="{{tab.name}}" select="getContent($index)" active="tab.active" disabled="tab.disabled">
+
           <div ng-hide="!tab.isLoaded">
             <div>
               <h4>{{tab.name}}</h4>
               <div>
                 <p>{{tab.tagline}}</p>
+                  {{tab.content}}
               </div>
             </div>
             <div>
