89b90a07b0321aba7167c5abc59a821e1485bf31
Alex Pronchuk <alex.pronchuk@ffwagency.com>
2018-01-25 18:53:29 +0200

    Colorbox fix.

--- docroot/sites/all/modules/features/nas_conservation_tracker/js/ct_tabs.js
+++ docroot/sites/all/modules/features/nas_conservation_tracker/js/ct_tabs.js
@@ -163,7 +163,7 @@
                           var match = tabData.settings.overview.youtube.match(regExp);
                           var yid = (match && match[7].length == 11) ? match[7] : false;
                           if (yid) {
-                            var youtube = 'https://www.youtube.com/embed/' + yid;
+                            var youtube = '//www.youtube.com/embed/' + yid;
                             tabData.settings.overview.preparedLink.class = 'colorbox-load youtube';
                             tabData.settings.overview.preparedLink.href = youtube + '?iframe=true&width=' + width + '&height=' + height;
                           }
@@ -171,6 +171,7 @@
                       }
                     }
                   }
+
                   $('.video-wrap a').attr('class', tabData.settings.overview.preparedLink.class).unbind('click');
 
                   // Add partners array to scope.
