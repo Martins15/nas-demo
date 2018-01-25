7416bb8758664d9ef002f7d6d5d293fb9ff43687
Martin Malovanets <martin.malovanets@ffwagency.com>
2018-01-25 12:32:55 +0200

    Revert "csv button style"
    This reverts commit abe8b0a63eb5e0bed40bfd2d7da2413f2fced918.

--- docroot/sites/all/modules/features/nas_conservation_tracker/templates/ct-scorecard-overview.tpl.php
+++ docroot/sites/all/modules/features/nas_conservation_tracker/templates/ct-scorecard-overview.tpl.php
@@ -37,8 +37,8 @@
     </div>
 </div>
 
-<div class="row scorecard-download-print bird-page body-item">
-  <div class="title-wrap__print columns large-4 hide-for-tiny hide-for-medium hide-for-small">
+<div class="row">
+  <div class="title-wrap__print  columns large-4  hide-for-tiny hide-for-medium hide-for-small">
     <a href="#" class="img-block print"></a> <a href="#" id="download-csv" class="img-block download">Download</a>
   </div>
 </div>
@@ -48,6 +48,7 @@
         <div class="js-preloader" ng-show="startLoad"></div>
         <div ng-repeat="(key, value) in tab.settings.tabs" tabs-repeat-directive>
             <div class="bird-page body-item {{value}}-tab-wrapper" ng-cloak  ng-class="{'active': isActive === value}">
+
                 <div class="title-wrap">
                     <div class="title-wrap__title columns large-8"><h3 ng-bind-html="tab.settings.overview.title"></h3></div>
                 </div>
