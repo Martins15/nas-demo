7d07ba23b9da230623daf21bb28b6571807157a0
andrew chernous <andrew.chernous@ffwagency.com>
2018-01-31 11:48:35 +0200

    ANW-1133 :: Update js for park select.

--- docroot/sites/all/modules/custom/nas_panes/plugins/content_types/park_select_bar/js/park_select_bar.js
+++ docroot/sites/all/modules/custom/nas_panes/plugins/content_types/park_select_bar/js/park_select_bar.js
@@ -6,39 +6,56 @@
       var parkSelect = $('#parks-select');
       var stateSelect = $('#state-select');
 
-
+      // Disable on load page second select.
       stateSelect.prop('disabled', 'disabled');
 
-      $.each(statesArray, function(index, val) {
-       var titleOption = val.title;
+      // Print all options for first select.
+      $.each(statesArray, function (index, val) {
+        var titleOption = val.title;
         parkSelect.append($('<option>', {
           value: titleOption,
-          text : titleOption
+          text: titleOption
         }));
       });
 
-      parkSelect.on('change', function() {
-        stateSelect.find('option').remove();
+      // If choose option in first select.
+      // Print all options for second select.
+      // Related first select.
+      parkSelect.change(function () {
+        // Clear all options but not first.
+        stateSelect.find('option:gt(0)').remove();
+        // Unable second select.
         stateSelect.prop('disabled', false);
         var currentState = this.value;
-        var getAllChild = $.map(statesArray, function(state) {
-          if (state.title === currentState) {return state;}
+        // Find options related first select.
+        var getAllChild = $.map(statesArray, function (state) {
+          if (state.title === currentState) {
+            return state;
+          }
         });
 
-        $.map(getAllChild, function(state) {
-          $.each(state.nodes, function(index, val) {
+        // Update second select.
+        $.map(getAllChild, function (state) {
+          $.each(state.nodes, function (index, val) {
             var titleOption = val.title;
             var aliasOption = val.alias;
             stateSelect.append($('<option>', {
               value: aliasOption,
-              text : titleOption
+              text: titleOption
             }));
           });
         });
       });
 
-      stateSelect.on('change', function() {
-       var getLink = $('option:selected', this ).attr('value');
+      // If choose option in second select.
+      stateSelect.on('change', function () {
+        // Get url form value.
+        var getLink = $('option:selected', this).attr('value');
+        // If we have some url.
+        // Go to this url.
+        if (getLink.length) {
+          window.location.href = location.origin + '/' + getLink;
+        }
       });
 
     }
