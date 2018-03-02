de0f09bcf85819aa8ad1be29c57c3efeca3b445b
Alex <alexey.belyaev@ffwagency.com>
2018-03-02 10:57:52 +0200

    [NAS-583] open native plants in new window

--- docroot/sites/all/modules/custom/native_plants_embed_form/native_plants_embed_form.js
+++ docroot/sites/all/modules/custom/native_plants_embed_form/native_plants_embed_form.js
@@ -121,7 +121,9 @@ var alterPost = function (args) {
     var isValidZip = /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(args.data.PostalCode);
     if (isValidZip && typeof args.data.EmailAddress === 'undefined') {
       document.getElementsByClassName('at-inner')[0].style.display = 'none';
-      location = '//www.audubon.org/native-plants/search?zipcode=' + args.data.PostalCode;
+      var url = '//www.audubon.org/native-plants/search?zipcode=' + args.data.PostalCode
+        , win = window.open(url, '_blank');
+      win.focus();
     }
   }
   return args;
@@ -132,7 +134,9 @@ nvtag_callbacks.alterPost.push(alterPost);
 
 var preSegue = function (args) {
   document.getElementsByClassName('at-inner')[0].style.display = 'none';
-  location = '//www.audubon.org/native-plants/search?zipcode=' + args.postVals.PostalCode;
+  var url = '//www.audubon.org/native-plants/search?zipcode=' + args.postVals.PostalCode
+    , win = window.open(url, '_blank');
+  win.focus();
 };
 nvtag_callbacks.preSegue = nvtag_callbacks.preSegue || [];
 nvtag_callbacks.preSegue.push(preSegue);
