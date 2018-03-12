d0693b17dfe7c06647e0048a650c96598a86d4e2
andrew chernous <andrew.chernous@ffwagency.com>
2018-03-12 18:15:15 +0200

    Add some style for “Parks Blog”.

--- docroot/sites/all/modules/custom/nas_panes/plugins/content_types/park_bird_trend/park-bird-trend.tpl.php
+++ docroot/sites/all/modules/custom/nas_panes/plugins/content_types/park_bird_trend/park-bird-trend.tpl.php
@@ -32,9 +32,6 @@
                       <p class="link__number"><?php print $data['amount'] ?></p>
                     </li>
                   <?php endforeach; ?>
-                  <li class="tabs-content__link link columns large-4">
-                    <div class="cell" id="chart"></div>
-                  </li>
                 </ul>
 
                 <?php foreach ($tabs['tabs'] as $tab => $data): ?>
@@ -46,64 +43,67 @@
               </div>
             </div>
           <?php endforeach; ?>
-          <script>
-
-            let birdDot = new StamenAudubonParks.BirdDots({
-              element: '#chart',
-              currentCounter: '#season_summer .current .link__number',
-              colonizedCounter: '#season_summer .extirpations .link__number',
-              extirpatedCounter: '#season_summer .colonizations .link__number',
-              dataUrl: 'sites/all/modules/features/nas_park_feature/csv/filtered_turnover_counts_53parks.csv',
-              park: 'Everglades National Park',
-              season: 'summer',
-              initStats: false,
-              onLoad: () => {
-              birdDot.animateIn(initUI)
-            }
-            });
-
-            function initUI() {
-              d3.select('.tabs-content__link.current').classed('selected', true);
-              d3.select('.tabs-content__link.extirpations').classed('selected', false);
-              d3.select('.tabs-content__link.colonizations').classed('selected', false);
-              birdDot.setCurrent();
-              //d3.select('.content').text('current birds');
-
-              //   d3.select('#stats-1').on('click', () => {
-              //     d3.select('#stats-1').classed('selected', true);
-              //   d3.select('#stats-2').classed('selected', false);
-              //   d3.select('#stats-3').classed('selected', false);
-              //   birdDot.setCurrent();
-              //   d3.select('.content').text('current birds');
-              // });
-              //
-              //   d3.select('#stats-2').on('click', () => {
-              //     d3.select('#stats-1').classed('selected', false);
-              //   d3.select('#stats-2').classed('selected', true);
-              //   d3.select('#stats-3').classed('selected', false);
-              //   birdDot.setColonized();
-              //   d3.select('.content').text('colonized birds');
-              // });
-              //
-              //   d3.select('#stats-3').on('click', () => {
-              //     d3.select('#stats-1').classed('selected', false);
-              //   d3.select('#stats-2').classed('selected', false);
-              //   d3.select('#stats-3').classed('selected', true);
-              //   birdDot.setExtirpated();
-              //   d3.select('.content').text('extirpated birds');
-              // });
-
-              //   d3.selectAll("input").on("change", () => {
-              //     let season = d3.select('input[name=season]:checked').attr('value')
-              //     birdDot.setSeason(season);
-              // });
-            }
-
-            //
-          </script>
           <?php print $view ?>
         </div>
       </div>
+        <div class="parks_blog">
+            <div class="cell" id="chart"></div>
+        </div>
+        <script>
+
+          let birdDot = new StamenAudubonParks.BirdDots({
+            element: '#chart',
+            currentCounter: '#season_summer .current .link__number',
+            colonizedCounter: '#season_summer .extirpations .link__number',
+            extirpatedCounter: '#season_summer .colonizations .link__number',
+            dataUrl: 'sites/all/modules/features/nas_park_feature/csv/filtered_turnover_counts_53parks.csv',
+            park: 'Everglades National Park',
+            season: 'summer',
+            initStats: false,
+            onLoad: () => {
+            birdDot.animateIn(initUI)
+          }
+          });
+
+          function initUI() {
+            d3.select('.tabs-content__link.current').classed('selected', true);
+            d3.select('.tabs-content__link.extirpations').classed('selected', false);
+            d3.select('.tabs-content__link.colonizations').classed('selected', false);
+            birdDot.setCurrent();
+            //d3.select('.content').text('current birds');
+
+            //   d3.select('#stats-1').on('click', () => {
+            //     d3.select('#stats-1').classed('selected', true);
+            //   d3.select('#stats-2').classed('selected', false);
+            //   d3.select('#stats-3').classed('selected', false);
+            //   birdDot.setCurrent();
+            //   d3.select('.content').text('current birds');
+            // });
+            //
+            //   d3.select('#stats-2').on('click', () => {
+            //     d3.select('#stats-1').classed('selected', false);
+            //   d3.select('#stats-2').classed('selected', true);
+            //   d3.select('#stats-3').classed('selected', false);
+            //   birdDot.setColonized();
+            //   d3.select('.content').text('colonized birds');
+            // });
+            //
+            //   d3.select('#stats-3').on('click', () => {
+            //     d3.select('#stats-1').classed('selected', false);
+            //   d3.select('#stats-2').classed('selected', false);
+            //   d3.select('#stats-3').classed('selected', true);
+            //   birdDot.setExtirpated();
+            //   d3.select('.content').text('extirpated birds');
+            // });
+
+            //   d3.selectAll("input").on("change", () => {
+            //     let season = d3.select('input[name=season]:checked').attr('value')
+            //     birdDot.setSeason(season);
+            // });
+          }
+
+          //
+        </script>
     </div>
   </div>
 </div>
