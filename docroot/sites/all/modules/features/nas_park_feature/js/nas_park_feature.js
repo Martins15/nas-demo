6338657fc0a58b3b704a091f112f6e2e88a97f28
Martin Malovanets <martin.malovanets@ffwagency.com>
2018-03-12 17:50:33 +0200

    Add map to landing page

--- docroot/sites/all/modules/features/nas_park_feature/js/nas_park_feature.js
+++ docroot/sites/all/modules/features/nas_park_feature/js/nas_park_feature.js
@@ -6,17 +6,34 @@
 (function ($, Drupal) {
   Drupal.behaviors.nasClimateTurnover = {
     attach: function (context, settings) {
-      var chart = new TurnoverChart({
-        element: '#' + settings.nasClimateFeature.elementId, // some container to render the chart inside of
-        dataUrl: settings.nasClimateFeature.dataUrl, //path to the static data file
-        park: settings.nasClimateFeature.parkTitle, // the name of the park to focus on
-        onClick: parkOnClick, // callback on clicking dots in the chart, function will be passed the name of the park and then the client side can open up that page's detail page.
-        season: 'summer' // the season to render
-      });
-
-      function parkOnClick(parkName) {
-        location = settings.nasClimateFeature.parkUrl;
+      if (settings.nasClimateFeature.turnoverChart) {
+        var chart = new StamenAudubonParks.TurnoverChart({
+          element: '#' + settings.nasClimateFeature.elementId,
+          dataUrl: settings.nasClimateFeature.dataUrl,
+          park: settings.nasClimateFeature.parkTitle,
+          onClick: parkOnClick,
+          season: 'summer'
+        });
       }
     }
+  }
+
+  Drupal.behaviors.nasClimateParkMap = {
+    attach: function (context, settings) {
+      if (settings.nasClimateFeature.parkMap) {
+        var parkMap = new StamenAudubonParks.ParkMap({
+          element: '#' + settings.nasClimateFeature.elementId,
+          dataUrl: settings.nasClimateFeature.dataUrl,
+          season: 'summer',
+          metric: 'colonization',
+          shapeUrl: settings.nasClimateFeature.shapeUrl,
+          onClick: parkOnClick
+        });
+      }
+    }
+  }
+
+  function parkOnClick(parkName) {
+    location = location.origin + '/' + Drupal.settings.nasClimateFeature.parkLinks[parkName];
   }
 })(jQuery, window.Drupal);
