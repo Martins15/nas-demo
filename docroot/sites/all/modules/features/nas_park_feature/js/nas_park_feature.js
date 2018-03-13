f24d7844d67a65c83506be84f6ec70b714d8cc26
Martin Malovanets <martin.malovanets@ffwagency.com>
2018-03-13 12:44:17 +0200

    rm test code

--- docroot/sites/all/modules/features/nas_park_feature/js/nas_park_feature.js
+++ docroot/sites/all/modules/features/nas_park_feature/js/nas_park_feature.js
@@ -38,66 +38,6 @@
   Drupal.behaviors.nasClimateParkMapSwitcher = {
     attach: function (context, settings) {
 
-      var tabsItem = $('.tab-slider--tabs', context)
-        , tabsNav = $('.tab-slider--nav li')
-        , tabsBody = $('.tab-slider--body', context)
-        , seasonSwitch = $('#edit-season')
-        , trendsSwitch = $('#edit-park-trend');
-
-
-      var seasonMap = {'1': 'season_summer', '2': 'season_winter'};
-
-      $('.switch-wrap').once('tab-init', function () {
-        var seasonCur = seasonMap[$(seasonSwitch).find('option:selected').val()];
-        $('.tab-slider--trigger').removeClass('active').filter('[rel="' + seasonCur + '"]').addClass('active');
-        if (seasonCur == 'season_winter') {
-          tabsItem.addClass('slide');
-        }
-        $('.tabs-content__link').removeClass('current').filter('[data-tab="display-' + $(trendsSwitch).find('option:selected').val() + '"]').addClass('current');
-        $('.tab-content').removeClass('current').filter('[data-content="display-' + $(trendsSwitch).find('option:selected').val() + '"]').addClass('current');
-      });
-
-      tabsNav.click(function () {
-        var seasonSwitch = $('#edit-season')
-          , formSubmit = $('#edit-submit-park-bird-trends');
-        tabsBody.hide();
-        var activeTab = $(this).attr("rel");
-        if ($(this).attr("rel") === "season_winter") {
-          tabsItem.addClass('slide');
-          $(seasonSwitch).find('option[value=2]').prop('selected', true);
-        }
-        else {
-          tabsItem.removeClass('slide');
-          $(seasonSwitch).find('option[value=1]').prop('selected', true);
-        }
-        $(formSubmit).click();
-        tabsNav.removeClass("active");
-        $(this).addClass("active");
-        $("#" + activeTab).fadeIn();
-        $('.tabs-content__link').removeClass('current').filter('[data-tab="display-' + $(trendsSwitch).find('option:selected').val() + '"]').addClass('current');
-        var season = activeTab.replace('season_', '');
-        if (Drupal.settings.nasClimateFeature && Drupal.settings.nasClimateFeature.chart) {
-          Drupal.settings.nasClimateFeature.chart.setSeason(season);
-        }
-      });
-
-      // Logic for tabs.
-      var tabWrap = $('ul.tabs-content', context);
-      $('li', tabWrap).click(function () {
-        var trendsSwitch = $('#edit-park-trend')
-          , formSubmit = $('#edit-submit-park-bird-trends');
-        var tabId = $(this).attr('data-tab');
-
-        $('li', tabWrap).removeClass('current');
-        $('.tab-content').removeClass('current');
-
-        $(this).addClass('current');
-
-        $(".container-data-tabs").find("[data-content='" + tabId + "']").addClass('current');
-
-        $(trendsSwitch).find('option[value="' + tabId.replace('display-', '') + '"]').prop('selected', true);
-        $(formSubmit).click();
-      });
     }
   }
 
